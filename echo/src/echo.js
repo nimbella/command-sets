dee9be661a61ea5839069e367a8b3a582e641fb1
Satya Rohith <me@satyarohith.com>
2020-01-03 16:11:18 +0530

    chore(echo): fix lint errors

--- echo/src/echo.js
+++ echo/src/echo.js
@@ -1,13 +1,15 @@
+// eslint-disable-next-line no-unused-vars
 async function _command(params, text, secrets = {}) {
-  const {
-    msg
-  } = params;
+  const {msg} = params;
 
   return {
-    response_type: 'in_channel', // or `ephemeral` for private response
+    // Or `ephemeral` for private response
+    response_type: 'in_channel', // eslint-disable-line camelcase
     text: msg
   };
 }
 
-const main = async ({__secrets = {}, text, ...params}) => ({body: await _command(params, text, __secrets)});
+const main = async ({__secrets = {}, text, ...params}) => ({
+  body: await _command(params, text, __secrets)
+});
 module.exports = main;
