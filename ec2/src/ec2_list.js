309fc9da9e5f161c5e2fe086f26f8fa6afac3766
Satya Rohith <me@satyarohith.com>
2020-01-03 16:04:04 +0530

    feat(ec2_list): list instances based on name

--- ec2/src/ec2_list.js
+++ ec2/src/ec2_list.js
@@ -17,6 +17,7 @@ async function _command(params, commandText, secrets = {}) {
   }
 
   const result = [];
+  const {substr = ''} = params;
   const aws = require('aws-sdk');
   const ec2 = new aws.EC2({
     apiVersion: '2016-11-15',
@@ -32,28 +33,68 @@ async function _command(params, commandText, secrets = {}) {
     const data = await describeInstancesAsync({DryRun: false});
 
     const {Instances} = data['Reservations'][0];
+
     for (const instance of Instances) {
-      result.push({
-        type: 'context',
-        elements: [
-          {
-            type: 'mrkdwn',
-            text: `ID: \`${instance.InstanceId}\``
-          },
-          {
-            type: 'mrkdwn',
-            text: `Type: \`${instance.InstanceType}\``
-          },
-          {
-            type: 'mrkdwn',
-            text: `State: \`${instance.State.Name}\``
-          },
-          {
-            type: 'mrkdwn',
-            text: `IP: \`${instance.PublicIpAddress}\``
+      if (substr.length > 0) {
+        // Retrieve the instance name.
+        let instanceName = '';
+        for (const tag of instance.Tags) {
+          if (tag.Key === 'Name') {
+            instanceName = tag.Value;
+            break;
           }
-        ]
-      });
+        }
+
+        if (instanceName.includes(substr)) {
+          result.push({
+            type: 'context',
+            elements: [
+              {
+                type: 'mrkdwn',
+                text: `ID: \`${instance.InstanceId}\``
+              },
+              {
+                type: 'mrkdwn',
+                text: `Name: ${instanceName}`
+              },
+              {
+                type: 'mrkdwn',
+                text: `Type: \`${instance.InstanceType}\``
+              },
+              {
+                type: 'mrkdwn',
+                text: `State: \`${instance.State.Name}\``
+              },
+              {
+                type: 'mrkdwn',
+                text: `IP: \`${instance.PublicIpAddress}\``
+              }
+            ]
+          });
+        }
+      } else {
+        result.push({
+          type: 'context',
+          elements: [
+            {
+              type: 'mrkdwn',
+              text: `ID: \`${instance.InstanceId}\``
+            },
+            {
+              type: 'mrkdwn',
+              text: `Type: \`${instance.InstanceType}\``
+            },
+            {
+              type: 'mrkdwn',
+              text: `State: \`${instance.State.Name}\``
+            },
+            {
+              type: 'mrkdwn',
+              text: `IP: \`${instance.PublicIpAddress}\``
+            }
+          ]
+        });
+      }
     }
   } catch (error) {
     result.push({
@@ -76,7 +117,6 @@ async function _command(params, commandText, secrets = {}) {
  * @property {string} text
  * @property {'in_channel'|'ephemeral'} [response_type]
  */
-
 const main = async ({__secrets = {}, commandText, ...params}) => ({
   body: await _command(params, commandText, __secrets)
 });
