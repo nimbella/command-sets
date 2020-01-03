3fd3985ec16a242fb3c44ed40b66e93d09583cc0
Satya Rohith <me@satyarohith.com>
2020-01-03 14:11:23 +0530

    feat(ec2): add ec2_status command

---
+++ ec2/src/ec2_status.js
@@ -0,0 +1,90 @@
+/**
+ * @description undefined
+ * @param {ParamsType} params list of command parameters
+ * @param {?string} commandText text message
+ * @param {!object} [secrets = {}] list of secrets
+ * @return {Promise<SlackBodyType>} Response body
+ */
+async function _command(params, commandText, secrets = {}) {
+  const {awsAccessKey, awsSecretKey, awsRegion} = secrets;
+
+  if (!awsAccessKey || !awsSecretKey || !awsRegion) {
+    return {
+      response_type: 'ephemeral',
+      text:
+        'You must create secrets for `awsAccessKey`, `awsSecretKey` and `awsRegion` to use this command '
+    };
+  }
+
+  const {id: instanceId} = params;
+  const result = [];
+  const aws = require('aws-sdk');
+  const ec2 = new aws.EC2({
+    apiVersion: '2016-11-15',
+    accessKeyId: awsAccessKey,
+    secretAccessKey: awsSecretKey,
+    region: awsRegion
+  });
+
+  try {
+    const {promisify} = require('util');
+    const describeInstanceStatusAsync = promisify(
+      ec2.describeInstanceStatus
+    ).bind(ec2);
+
+    const data = await describeInstanceStatusAsync({
+      DryRun: false,
+      InstanceIds: [instanceId]
+    });
+
+    const {InstanceStatuses} = data;
+
+    for (const instance of InstanceStatuses) {
+      result.push({
+        type: 'context',
+        elements: [
+          {
+            type: 'mrkdwn',
+            text: `ID: \`${instance.InstanceId}\``
+          },
+          {
+            type: 'mrkdwn',
+            text: `State: \`${instance.InstanceState.Name}\``
+          },
+          {
+            type: 'mrkdwn',
+            text: `InstanceStatus: \`${instance.InstanceStatus.Status}\``
+          },
+          {
+            type: 'mrkdwn',
+            text: `SystemStatus: \`${instance.SystemStatus.Status}\``
+          }
+        ]
+      });
+    }
+  } catch (error) {
+    result.push({
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `*ERROR:* ${error.message}`
+      }
+    });
+  }
+
+  return {
+    response_type: 'in_channel',
+    blocks: result
+  };
+}
+
+/**
+ * @typedef {object} SlackBodyType
+ * @property {string} text
+ * @property {'in_channel'|'ephemeral'} [response_type]
+ */
+
+const main = async ({__secrets = {}, commandText, ...params}) => ({
+  body: await _command(params, commandText, __secrets)
+});
+module.exports = main;
