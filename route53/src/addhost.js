78602e847a9b337b9414d1e89d07c1b59997b65a
Satya Rohith <me@satyarohith.com>
2020-01-03 16:45:18 +0530

    refactor(route53): improve code & fix lint errors

--- route53/src/addhost.js
+++ route53/src/addhost.js
@@ -1,15 +1,15 @@
 // jshint esversion: 9
 
 async function _command(params, commandText, secrets = {}) {
-  const {
-    hostname,
-    ip_address
-  } = params;
-  
-  if (!secrets.route53AccessKey || !secrets.route53SecretKey || !secrets.route53ZoneId) {
+  const {hostname, ip_address: ipAddress} = params;
+  const {route53AccessKey, route53SecretKey, route53ZoneId} = secrets;
+
+  if (!route53AccessKey || !route53SecretKey || !route53ZoneId) {
     return {
-      response_type: 'in_channel', // or `ephemeral` for private response
-      text: "You must create secrets for route53AccessKey, route53SecretKey and route53ZoneId to use this command"
+      // Or `ephemeral` for private response
+      response_type: 'in_channel', // eslint-disable-line camelcase
+      text:
+        'You must create secrets for route53AccessKey, route53SecretKey and route53ZoneId to use this command'
     };
   }
 
@@ -20,37 +20,47 @@ async function _command(params, commandText, secrets = {}) {
     secretAccessKey: secrets.route53SecretKey
   });
 
-  let changeParams = {
-     "HostedZoneId": '/hostedzone/' + secrets.route53ZoneId,
-     "ChangeBatch": {
-       "Changes": [
-         {
-           "Action": "UPSERT",
-           "ResourceRecordSet": {
-             "Name": hostname,
-             "Type": 'A',
-             "TTL": 299,
-             "ResourceRecords": [ { "Value": ip_address } ]
-           }
-         }
-       ]
-     }
-   };
-  return route53.changeResourceRecordSets(changeParams).promise().then(
-    function(data) {
-      return {
-        "response_type": "in_channel",
-        "text": 'hostname ' + hostname + ' added. Route53 status: ' + data.ChangeInfo.Status
-      };
-    },
-    function(err) {
-      return {
-        "response_type": "in_channel",
-        "text": "Error: " + JSON.stringify(err)
-      };
+  const changeParams = {
+    HostedZoneId: '/hostedzone/' + secrets.route53ZoneId,
+    ChangeBatch: {
+      Changes: [
+        {
+          Action: 'UPSERT',
+          ResourceRecordSet: {
+            Name: hostname,
+            Type: 'A',
+            TTL: 299,
+            ResourceRecords: [{Value: ipAddress}]
+          }
+        }
+      ]
     }
-  );
+  };
+
+  return route53
+    .changeResourceRecordSets(changeParams)
+    .promise()
+    .then(
+      function(data) {
+        return {
+          response_type: 'in_channel', // eslint-disable-line camelcase
+          text:
+            'hostname ' +
+            hostname +
+            ' added. Route53 status: ' +
+            data.ChangeInfo.Status
+        };
+      },
+      function(err) {
+        return {
+          response_type: 'in_channel', // eslint-disable-line camelcase
+          text: 'Error: ' + JSON.stringify(err)
+        };
+      }
+    );
 }
 
-const main = async ({__secrets = {}, commandText, ...params}) => ({body: await _command(params, commandText, __secrets)});
+const main = async ({__secrets = {}, commandText, ...params}) => ({
+  body: await _command(params, commandText, __secrets)
+});
 module.exports = main;
--- route53/src/listhosts.js
+++ route53/src/listhosts.js
@@ -2,33 +2,36 @@
 
 function compactResponse(data, type, match) {
   let title = 'All route53 ';
-  if (!type) {
-    title += 'hostnames';
-  } else {
+  if (type) {
     title += ' ' + type + ' records';
-  }
-  if (!match) {
-    title += ':';
   } else {
-    title += ' containing "' + match + '":';
+    title += 'hostnames';
   }
-  let response =  {
-    "response_type": "in_channel",
-    "attachments":
-      [ {
-      "title": title,
-      "color": "#764FA5"
-      } ]
+
+  if (match) {
+    title += ' containing "' + match + '":';
+  } else {
+    title += ':';
+  }
+
+  const response = {
+    response_type: 'in_channel', // eslint-disable-line camelcase
+    attachments: [
+      {
+        title,
+        color: '#764FA5'
+      }
+    ]
   };
 
-  let rr = data.ResourceRecordSets;
-  let fields = [];
-  for (var i in rr) {
-    if (type == undefined || type == rr[i].Type) {
-      let name = rr[i].Name.replace('\\052', '*');
-      let value = rr[i].ResourceRecords[0].Value;
+  const rr = data.ResourceRecordSets;
+  const fields = [];
+  for (const i in rr) {
+    if (type === undefined || type === rr[i].Type) {
+      const name = rr[i].Name.replace('\\052', '*');
+      const value = rr[i].ResourceRecords[0].Value;
       if (!match || name.includes(match)) {
-        fields.push( { "title": name, "value": value, "short": true } );
+        fields.push({title: name, value, short: true});
       }
     }
   }
@@ -38,42 +41,45 @@ function compactResponse(data, type, match) {
 }
 
 async function _command(params, commandText, secrets = {}) {
-  const {
-    type = false,
-    record_type,
-    match = false,
-    match_string
-  } = params;
+  const {record_type: recordType, match_string: matchString} = params;
+  const {route53AccessKey, route53SecretKey, route53ZoneId} = secrets;
 
-  if (!secrets.route53AccessKey || !secrets.route53SecretKey || !secrets.route53ZoneId) {
+  if (!route53AccessKey || !route53SecretKey || !route53ZoneId) {
     return {
-      response_type: 'in_channel', // or `ephemeral` for private response
-      text: "You must create secrets for route53AccessKey, route53SecretKey and route53ZoneId to use this command"
+      // Or `ephemeral` for private response
+      response_type: 'in_channel', // eslint-disable-line camelcase
+      text:
+        'You must create secrets for route53AccessKey, route53SecretKey and route53ZoneId to use this command'
     };
   }
 
   const AWS = require('aws-sdk');
 
-  // initialize with AWS.Route53() instead of AWS config for better peformance
+  // Initialize with AWS.Route53() instead of AWS config for better peformance
   const route53 = new AWS.Route53({
     accessKeyId: secrets.route53AccessKey,
     secretAccessKey: secrets.route53SecretKey
   });
 
-  // returns a promise which will be resolved before the function returns
-  const zoneParams = { HostedZoneId: '/hostedzone/' + secrets.route53ZoneId };
-  return route53.listResourceRecordSets(zoneParams).promise().then(
-    function(data) {
-      return compactResponse(data, record_type, match_string);
-    },
-    function(err) {
-      return {
-        response_type: 'in_channel',
-        text: "Error: " + err
-      };
-    }
-  );
+  // Returns a promise which will be resolved before the function returns
+  const zoneParams = {HostedZoneId: '/hostedzone/' + secrets.route53ZoneId};
+  return route53
+    .listResourceRecordSets(zoneParams)
+    .promise()
+    .then(
+      function(data) {
+        return compactResponse(data, recordType, matchString);
+      },
+      function(err) {
+        return {
+          response_type: 'in_channel', // eslint-disable-line camelcase
+          text: 'Error: ' + err
+        };
+      }
+    );
 }
 
-const main = async ({__secrets = {}, commandText, ...params}) => ({body: await _command(params, commandText, __secrets)});
+const main = async ({__secrets = {}, commandText, ...params}) => ({
+  body: await _command(params, commandText, __secrets)
+});
 module.exports = main;
