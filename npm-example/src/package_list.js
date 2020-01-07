22a0a9ae79a32c7c3de5a06f7f12b44203b4ad62
Satya Rohith <me@satyarohith.com>
2020-01-07 14:24:47 +0530

    chore(npm-example): fix lint errors

--- npm-example/src/package_list.js
+++ npm-example/src/package_list.js
@@ -2,44 +2,57 @@
 
 let readInstalled;
 
-// installs a set of npm packges
+// Installs a set of npm packges
 async function install(pkgs) {
   pkgs = pkgs.join(' ');
   return new Promise((resolve, reject) => {
-    const { exec } = require('child_process');
-    exec(`npm install ${pkgs}`, (err, stdout, stderr) => {
+    const {exec} = require('child_process');
+    exec(`npm install ${pkgs}`, err => {
       if (err) reject(err);
       else resolve();
     });
   });
 }
 
-async function get_package_list() {
+async function getPackageList() {
   return new Promise((resolve, reject) => {
-    readInstalled('/', { depth: 0, dev: true }, (err, pkgs) => {
-      let list = Object.entries(pkgs.dependencies).map(([k, v]) => `${k}: ${v.homepage}`);
-      let str  = 'packages:\n' + list.join('\n');
+    readInstalled('/', {depth: 0, dev: true}, (err, pkgs) => {
+      if (err) reject(err);
+
+      const list = Object.entries(pkgs.dependencies).map(
+        ([k, v]) => `${k}: ${v.homepage}`
+      );
+      const str = 'packages:\n' + list.join('\n');
       resolve(str);
     });
   });
 }
 
+// eslint-disable-next-line no-unused-vars
 async function _command(params, commandText, secrets = {}) {
   // We keep the package in a global for performance reasons. Globals are kept when a
   // container is re-used so keeping the package global means it will only be loaded when
   // a container is initialized
 
-  if (!readInstalled) {
-    await install([ 'read-installed' ]);
-    readInstalled = require('read-installed');
+  let str = '';
+  try {
+    if (!readInstalled) {
+      await install(['read-installed']);
+      readInstalled = require('read-installed');
+    }
+
+    str = await getPackageList();
+  } catch (error) {
+    str = error.message;
   }
 
-  let str = await get_package_list();
   return {
-    response_type: 'in_channel',
+    response_type: 'in_channel', // eslint-disable-line camelcase
     text: str
   };
 }
 
-const main = async ({__secrets = {}, commandText, ...params}) => ({body: await _command(params, commandText, __secrets)});
+const main = async ({__secrets = {}, commandText, ...params}) => ({
+  body: await _command(params, commandText, __secrets)
+});
 module.exports = main;
