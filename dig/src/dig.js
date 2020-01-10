a6ca463f3cc4bc9b419cd8ed2cb4917822f9c7c4
Satya Rohith <me@satyarohith.com>
2020-01-10 16:49:25 +0530

    refactor: use mui

--- dig/src/dig.js
+++ dig/src/dig.js
@@ -1,41 +1,62 @@
 'use strict';
 
 /**
+ * A small function that converts slack elements `context` and `section` to mattermost compatible markdown.
+ * @param {object} element - Slack element
+ * @param {string} client - name of the client
+ */
+const mui = (element, client) => {
+  const output = [];
+  if (client === 'slack') {
+    return element;
+  } else {
+    if (element.type === 'context') {
+      for (const item of element.elements) {
+        output.push(item.text.replace(/\*/g, '**'));
+      }
+    } else if (element.type === 'section') {
+      output.push(element.text.text.replace(/\*/g, '**'));
+    }
+  }
+
+  return output.join(' ');
+};
+
+/**
  * Formats 'A' records into slack blocks or mattermost markdown.
  * @param {array} records - Array returned by the resolve func.
  * @param {string} hostname - The hostname for which the request is made.
  * @returns {array} - An array of formatted slack blocks.
  */
-const formatARecords = (records, {hostname, isSlack}) => {
+const formatARecords = (records, {hostname, client}) => {
   const output = [];
   for (const record of records) {
-    if (isSlack) {
-      output.push({
-        type: 'context',
-        elements: [
-          {
-            type: 'mrkdwn',
-            text: `${hostname}`
-          },
-          {
-            type: 'mrkdwn',
-            text: `Type: *A*`
-          },
-          {
-            type: 'mrkdwn',
-            text: `TTL: ${record.ttl}`
-          },
-          {
-            type: 'mrkdwn',
-            text: `IP: \`${record.address}\``
-          }
-        ]
-      });
-    } else {
-      output.push(
-        `${hostname} Type: **A** TTL: \`${record.ttl}\` IP: \`${record.address}\``
-      );
-    }
+    output.push(
+      mui(
+        {
+          type: 'context',
+          elements: [
+            {
+              type: 'mrkdwn',
+              text: `${hostname}`
+            },
+            {
+              type: 'mrkdwn',
+              text: `Type: *A*`
+            },
+            {
+              type: 'mrkdwn',
+              text: `TTL: ${record.ttl}`
+            },
+            {
+              type: 'mrkdwn',
+              text: `IP: \`${record.address}\``
+            }
+          ]
+        },
+        client
+      )
+    );
   }
 
   return output;
@@ -47,36 +68,35 @@ const formatARecords = (records, {hostname, isSlack}) => {
  * @param {string} hostname - The hostname to which the request is made.
  * @returns {array} - An array of formatted slack blocks.
  */
-const formatAAAARecords = (records, hostname) => {
+const formatAAAARecords = (records, {hostname, client}) => {
   const output = [];
   for (const record of records) {
-    if (isSlack) {
-      output.push({
-        type: 'context',
-        elements: [
-          {
-            type: 'mrkdwn',
-            text: `${hostname}`
-          },
-          {
-            type: 'mrkdwn',
-            text: `Type: *AAAA*`
-          },
-          {
-            type: 'mrkdwn',
-            text: `TTL: ${record.ttl}`
-          },
-          {
-            type: 'mrkdwn',
-            text: `IP: \`${record.address}\``
-          }
-        ]
-      });
-    } else {
-      output.push(
-        `${hostname} Type: **AAAA** TTL: \`${record.ttl}\` IP: \`${record.address}\``
-      );
-    }
+    output.push(
+      mui(
+        {
+          type: 'context',
+          elements: [
+            {
+              type: 'mrkdwn',
+              text: `${hostname}`
+            },
+            {
+              type: 'mrkdwn',
+              text: `Type: *AAAA*`
+            },
+            {
+              type: 'mrkdwn',
+              text: `TTL: ${record.ttl}`
+            },
+            {
+              type: 'mrkdwn',
+              text: `IP: \`${record.address}\``
+            }
+          ]
+        },
+        client
+      )
+    );
   }
 
   return output;
@@ -88,23 +108,22 @@ const formatAAAARecords = (records, hostname) => {
  * @param {string} hostname - The hostname to which the request is made.
  * @returns {array} - An array of formatted slack blocks.
  */
-const formatMXRecords = (records, {hostname, isSlack}) => {
+const formatMXRecords = (records, {hostname, client}) => {
   const output = [];
   for (const record of records) {
-    if (isSlack) {
-      output.push({
-        type: 'context',
-        elements: [
-          {type: 'mrkdwn', text: `${hostname}`},
-          {type: 'mrkdwn', text: `\`${record.exchange}\``},
-          {type: 'mrkdwn', text: `Priority: \`${record.priority}\``}
-        ]
-      });
-    } else {
-      output.push(
-        `${hostname} \`${record.exchange}\` Priority: \`${record.priority}\``
-      );
-    }
+    output.push(
+      mui(
+        {
+          type: 'context',
+          elements: [
+            {type: 'mrkdwn', text: `${hostname}`},
+            {type: 'mrkdwn', text: `\`${record.exchange}\``},
+            {type: 'mrkdwn', text: `Priority: \`${record.priority}\``}
+          ]
+        },
+        client
+      )
+    );
   }
 
   return output;
@@ -116,21 +135,22 @@ const formatMXRecords = (records, {hostname, isSlack}) => {
  * @param {string} hostname - The hostname to which the request is made.
  * @returns {array} - An array of formatted slack blocks.
  */
-const formatTXTRecords = (records, {hostname, isSlack}) => {
+const formatTXTRecords = (records, {hostname, client}) => {
   const output = [];
   for (const record of records) {
-    if (isSlack) {
-      output.push({
-        type: 'context',
-        elements: [
-          {type: 'mrkdwn', text: `${hostname}`},
-          {type: 'mrkdwn', text: `Type: *TXT*`},
-          {type: 'mrkdwn', text: `\`${record[0]}\``}
-        ]
-      });
-    } else {
-      output.push(`${hostname} Type: **TXT** \`${record[0]}\``);
-    }
+    output.push(
+      mui(
+        {
+          type: 'context',
+          elements: [
+            {type: 'mrkdwn', text: `${hostname}`},
+            {type: 'mrkdwn', text: `Type: *TXT*`},
+            {type: 'mrkdwn', text: `\`${record[0]}\``}
+          ]
+        },
+        client
+      )
+    );
   }
 
   return output;
@@ -142,42 +162,38 @@ const formatTXTRecords = (records, {hostname, isSlack}) => {
  * @param {string} hostname - The hostname to which the request is made.
  * @returns {array} - An array of formatted slack blocks.
  */
-const formatNSRecords = (records, {hostname, isSlack}) => {
+const formatNSRecords = (records, {hostname, client}) => {
   const output = [];
   for (const record of records) {
-    if (isSlack) {
-      output.push({
-        type: 'context',
-        elements: [
-          {type: 'mrkdwn', text: `${hostname}`},
-          {type: 'mrkdwn', text: `Type: *NS*`},
-          {type: 'mrkdwn', text: `\`${record}\``}
-        ]
-      });
-    } else {
-      output.push(`${hostname} Type: **NS** \`${record}\``);
-    }
+    output.push(
+      mui(
+        {
+          type: 'context',
+          elements: [
+            {type: 'mrkdwn', text: `${hostname}`},
+            {type: 'mrkdwn', text: `Type: *NS*`},
+            {type: 'mrkdwn', text: `\`${record}\``}
+          ]
+        },
+        client
+      )
+    );
   }
 
   return output;
 };
 
-const formatSoaRecord = (record, {hostname, isSlack}) => {
-  const output = [`**${hostname}**`];
+const formatSoaRecord = (record, {hostname, client}) => {
   const block = {
     type: 'context',
     elements: [{type: 'mrkdwn', text: `*${hostname}*`}]
   };
 
   for (const [key, value] of Object.entries(record)) {
-    if (isSlack) {
-      block.elements.push({type: 'mrkdwn', text: `${key}: \`${value}\``});
-    } else {
-      output.push(`${key}: \`${value}\``);
-    }
+    block.elements.push({type: 'mrkdwn', text: `${key}: \`${value}\``});
   }
 
-  return isSlack ? [block] : [output.join(' ')];
+  return [mui(block, client)];
 };
 
 /**
@@ -189,7 +205,15 @@ const formatSoaRecord = (record, {hostname, isSlack}) => {
  */
 async function _command(params) {
   const {hostname, __slack_headers: clientHeaders} = params;
-  const isSlack = () => clientHeaders['user-agent'].includes('Slackbot');
+  const getClient = () => {
+    if (clientHeaders['user-agent'].includes('Slackbot')) {
+      return 'slack';
+    }
+
+    return 'mattermost';
+  };
+
+  const client = getClient();
 
   let {type = 'A'} = params;
   type = type.toUpperCase();
@@ -203,52 +227,42 @@ async function _command(params) {
       case 'A': {
         const resolve4Async = promisify(dns.resolve4);
         const records = await resolve4Async(hostname, {ttl: true});
-        result.push(...formatARecords(records, {hostname, isSlack: isSlack()}));
+        result.push(...formatARecords(records, {hostname, client}));
         break;
       }
 
       case 'AAAA': {
         const resolve6Async = promisify(dns.resolve6);
         const records = await resolve6Async(hostname, {ttl: true});
-        result.push(
-          ...formatAAAARecords(records, {hostname, isSlack: isSlack()})
-        );
+        result.push(...formatAAAARecords(records, {hostname, client}));
         break;
       }
 
       case 'TXT': {
         const resolveTXTAsync = promisify(dns.resolveTxt);
         const records = await resolveTXTAsync(hostname);
-        result.push(
-          ...formatTXTRecords(records, {hostname, isSlack: isSlack()})
-        );
+        result.push(...formatTXTRecords(records, {hostname, client}));
         break;
       }
 
       case 'MX': {
         const resolveMXAsync = promisify(dns.resolveMx);
         const records = await resolveMXAsync(hostname);
-        result.push(
-          ...formatMXRecords(records, {hostname, isSlack: isSlack()})
-        );
+        result.push(...formatMXRecords(records, {hostname, client}));
         break;
       }
 
       case 'NS': {
         const resolveNSAsync = promisify(dns.resolveNs);
         const records = await resolveNSAsync(hostname);
-        result.push(
-          ...formatNSRecords(records, {hostname, isSlack: isSlack()})
-        );
+        result.push(...formatNSRecords(records, {hostname, client}));
         break;
       }
 
       case 'SOA': {
         const resolveSoaAsync = promisify(dns.resolveSoa);
         const records = await resolveSoaAsync(hostname);
-        result.push(
-          ...formatSoaRecord(records, {hostname, isSlack: isSlack()})
-        );
+        result.push(...formatSoaRecord(records, {hostname, client}));
         break;
       }
 
@@ -267,51 +281,55 @@ async function _command(params) {
     }
   } catch (error) {
     if (error.code === 'ENODATA') {
-      if (isSlack()) {
-        result.push({
-          type: 'context',
-          elements: [
-            {
-              type: 'mrkdwn',
-              text: `No records of type *${type}* found for ${hostname}.`
-            }
-          ]
-        });
-      } else {
-        result.push(`No records of type **${type}** found for ${hostname}`);
-      }
+      result.push(
+        mui(
+          {
+            type: 'context',
+            elements: [
+              {
+                type: 'mrkdwn',
+                text: `No records of type *${type}* found for ${hostname}.`
+              }
+            ]
+          },
+          client
+        )
+      );
     } else if (error.code === 'ENOTFOUND') {
-      if (isSlack()) {
-        result.push({
-          type: 'context',
-          elements: [
-            {
-              type: 'mrkdwn',
-              text: `Domain ${hostname} not found.`
-            }
-          ]
-        });
-      } else {
-        result.push(`Domain ${hostname} not found.`);
-      }
+      result.push(
+        mui(
+          {
+            type: 'context',
+            elements: [
+              {
+                type: 'mrkdwn',
+                text: `Domain ${hostname} not found.`
+              }
+            ]
+          },
+          client
+        )
+      );
     } else {
-      if (isSlack()) {
-        result.push({
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: `*ERROR:* ${error.message}`
-          }
-        });
-      } else {
-        result.push(`**ERROR:** ${error.message}`);
-      }
+      result.push(
+        mui(
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `*ERROR:* ${error.message}`
+            }
+          },
+          client
+        )
+      );
     }
   }
 
   return {
     response_type: 'in_channel', // eslint-disable-line camelcase
-    [isSlack() ? 'blocks' : 'text']: isSlack() ? result : result.join('\n')
+    [client === 'slack' ? 'blocks' : 'text']:
+      client === 'slack' ? result : result.join('\n')
   };
 }
 
