25267172fe488e9a35a6c2e626b53835acbc8edf
Satya Rohith <me@satyarohith.com>
2020-01-29 15:53:41 +0530

    [billing] Improve the output of datadogbill (#29)
    * feat: use slack blocks & refactor a bit
    
    * fix(run): return message when no secrets
    
    * feat: port to mattermost

--- billing/src/datadogbill.js
+++ billing/src/datadogbill.js
@@ -1,28 +1,67 @@
 // jshint esversion: 8
 
+/**
+ * A small function that converts slack elements `context` and `section` to mattermost compatible markdown.
+ * @param {object} element - Slack element
+ * @param {string} client - name of the client
+ */
+const mui = (element, client) => {
+  if (client === 'slack') {
+    return element;
+  }
+
+  const output = [];
+  switch (element.type) {
+    case 'context': {
+      for (const item of element.elements) {
+        output.push(item.text.replace(/\*/g, '**'));
+      }
+      break;
+    }
+    case 'section': {
+      if (element.fields && element.fields.length > 0) {
+        for (const field of element.fields) {
+          output.push(field.text.replace(/\*/g, '**') + '\n');
+        }
+      } else if (element.text) {
+        // Convert single text element to h4 in mattermost.
+        output.push('#### ' + element.text.text.replace(/\*/g, '**'));
+      }
+      break;
+    }
+    case 'divider': {
+      output.push('***');
+      break;
+    }
+  }
+
+  return output.join(' ');
+};
+
 // this code is from: https://www.tomas-dvorak.cz/posts/nodejs-request-without-dependencies/
 // it allows us to do a promise https request without any dependencies
-
 const getContent = function(url) {
   // return new pending promise
   return new Promise((resolve, reject) => {
     // select http or https module, depending on reqested url
     const lib = url.startsWith('https') ? require('https') : require('http');
-    const request = lib.get(url, (response) => {
+    const request = lib.get(url, response => {
       // handle http errors
       if (response.statusCode < 200 || response.statusCode > 299) {
-         reject(new Error('Failed to load page, status code: ' + response.statusCode));
-       }
+        reject(
+          new Error('Failed to load page, status code: ' + response.statusCode)
+        );
+      }
       // temporary data holder
       const body = [];
       // on every content chunk, push it to the data array
-      response.on('data', (chunk) => body.push(chunk));
+      response.on('data', chunk => body.push(chunk));
       // we are done, resolve promise with those joined chunks
       response.on('end', () => resolve(body.join('')));
     });
     // handle connection errors of the request
-    request.on('error', (err) => reject(err));
-    });
+    request.on('error', err => reject(err));
+  });
 };
 
 //
@@ -51,24 +90,25 @@ const getContent = function(url) {
 // And Datadog has a rate limit of 60 requests per hour per-API key to their usage API at this time
 //
 function calcHostsCosts(json) {
-  let numHours = json.usage.length;
-  if (numHours == 0) {
-    return { cost: 0, forwardCost: 0, text: "no data" };
+  const numHours = json.usage.length;
+  if (numHours === 0) {
+    return {cost: 0, forwardCost: 0};
   }
 
+  const hostsByHour = [];
+  const apmHostsByHour = [];
+
   let numBadHosts = 0;
-  let hostsByHour = [];
-  let apmHostsByHour = [];
   let recentHostCount = 0;
   let recentApmHostCount = 0;
-  let i;
-  for (i = 0; i < numHours; i++) {
-    let hostCount = json.usage[i].host_count;
-    let apmHostCount = json.usage[i].apm_host_count;
+
+  for (const usage of json.usage) {
+    const hostCount = usage.host_count;
+    const apmHostCount = usage.apm_host_count;
 
     // datadog's usage logging has problems where recent host fields are null for unknown reasons
     if (!hostCount || !apmHostCount) {
-      numBadHosts = numBadHosts + 1;
+      numBadHosts += 1;
     } else {
       hostsByHour.push(hostCount);
       recentHostCount = hostCount;
@@ -76,123 +116,245 @@ function calcHostsCosts(json) {
       recentApmHostCount = apmHostCount;
     }
   }
-  let maxHostCount = Math.max(...hostsByHour);
-  let maxApmHostCount = Math.max(...apmHostsByHour);
+
+  const maxHostCount = Math.max(...hostsByHour);
+  const maxApmHostCount = Math.max(...apmHostsByHour);
 
   hostsByHour.sort((a, b) => b - a);
   apmHostsByHour.sort((a, b) => b - a);
-  let n99 = Math.floor(numHours * 0.01);
-  let billingHostCount =  hostsByHour[n99];
-  let billingApmHostCount = apmHostsByHour[n99];
+  const n99 = Math.floor(numHours * 0.01);
+  const billingHostCount = hostsByHour[n99];
+  const billingApmHostCount = apmHostsByHour[n99];
 
-  let cost = billingHostCount * 18 + billingApmHostCount * 36;
-  let forwardCost = recentHostCount * 18 + recentApmHostCount * 36;
+  const cost = billingHostCount * 18 + billingApmHostCount * 36;
+  const forwardCost = recentHostCount * 18 + recentApmHostCount * 36;
 
-  return { cost: cost, forwardCost: forwardCost, text: "numBadHosts=" + numBadHosts + " billingHostCount=" + billingHostCount +
-   " billingApmHostCount=" + billingApmHostCount + " maxHostCount=" + maxHostCount +
-   " recentHostCount=" + recentHostCount + " recentApmHostCount=" + recentApmHostCount +
-   " maxApmHostCount=" + maxApmHostCount + " numHours=" + numHours + " 99th=#" + n99 };
+  return {
+    cost,
+    forwardCost,
+    numBadHosts,
+    billingHostCount,
+    billingApmHostCount,
+    maxHostCount,
+    recentHostCount,
+    recentApmHostCount,
+    maxApmHostCount,
+    numHours,
+    n99
+  };
 }
 
 function calcMetricsCosts(json) {
-  numHours = json.usage.length;
-  if (numHours == 0) {
-    return { cost: 0, forwardCost: 0, text: "no data" };
+  const numHours = json.usage.length;
+  if (numHours === 0) {
+    return {cost: 0, forwardCost: 0};
   }
 
-  // for debugging:
-  // console.log(JSON.stringify(json, null, 4));
+  const metricsByHour = [];
 
-  let metricsByHour = [];
-  let i;
-  for (i = 0; i < numHours; i++) {
+  for (let i = 0; i < numHours; i++) {
     metricsByHour.push(json.usage[i].num_custom_timeseries);
   }
-  let maxMetricsCount = Math.max(...metricsByHour);
-  let recentMetricsCount = metricsByHour[numHours - 1];
+  const maxMetricsCount = Math.max(...metricsByHour);
+  const recentMetricsCount = metricsByHour[numHours - 1];
 
   metricsByHour.sort((a, b) => b - a);
-  let n99 = Math.floor(numHours * 0.01);
-  let billingMetricsCount =  metricsByHour[n99];
+  const n99 = Math.floor(numHours * 0.01);
+  const billingMetricsCount = metricsByHour[n99];
 
-  let cost = billingMetricsCount * 0.05;
-  let forwardCost = recentMetricsCount * 0.05;
+  const cost = billingMetricsCount * 0.05;
+  const forwardCost = recentMetricsCount * 0.05;
 
-  return { cost: cost, forwardCost: forwardCost, text: "metricsCost=" + cost.toFixed(2) + " forwardMetricsCost=" + forwardCost.toFixed(2) + " billingMetrics=" + billingMetricsCount + " recentMetrics=" + recentMetricsCount + " maxMetricsCount=" + maxMetricsCount };
+  return {
+    cost,
+    forwardCost,
+    billingMetricsCount,
+    recentMetricsCount,
+    maxMetricsCount
+  };
 }
 
 function calcSyntheticsCosts(json) {
-  numHours = json.usage.length;
-  if (numHours == 0) {
-    return { cost: 0, forwardCost: 0, text: "no data" };
+  const numHours = json.usage.length;
+  if (numHours === 0) {
+    return {cost: 0, forwardCost: 0};
   }
 
   let recentCount = 0;
   let totalSynthetics = 0;
-  for (i = 0; i < numHours; i++) {
-    let c = json.usage[i].check_calls_count;
-    totalSynthetics += c;
-    recentCount = c;
+  for (let i = 0; i < numHours; i++) {
+    const count = json.usage[i].check_calls_count;
+    totalSynthetics += count;
+    recentCount = count;
   }
 
-  let cost = (totalSynthetics / 1000) * 7.2 * (720 / numHours);
-  let forwardCost = (recentCount * 24 * 30) / 1000 * 7.2;
+  const cost = (totalSynthetics / 1000) * 7.2 * (720 / numHours);
+  const forwardCost = ((recentCount * 24 * 30) / 1000) * 7.2;
 
-  return { cost: cost, forwardCost: forwardCost, text: "numSyn=" + totalSynthetics + " synCost=" + cost.toFixed(2) + " forwardSynCost=" + forwardCost.toFixed(2) + " recentSyn=" + recentCount };
+  return {
+    cost,
+    forwardCost,
+    totalSynthetics,
+    recentCount
+  };
 }
 
 function calcCosts(hostsJson, timeseriesJson, syntheticsJson) {
-    let hostsCosts = calcHostsCosts(hostsJson);
-    let metricsCosts = calcMetricsCosts(timeseriesJson);
-    let syntheticsCosts = calcSyntheticsCosts(syntheticsJson);
+  const verbose = {};
+  const hostsCosts = calcHostsCosts(hostsJson);
+  const metricsCosts = calcMetricsCosts(timeseriesJson);
+  const syntheticsCosts = calcSyntheticsCosts(syntheticsJson);
 
-    let cost = hostsCosts.cost +  metricsCosts.cost + syntheticsCosts.cost;
-    let forwardCost = hostsCosts.forwardCost +  metricsCosts.forwardCost + syntheticsCosts.forwardCost;
-
-    let detail = hostsCosts.text + " " + metricsCosts.text + " " + syntheticsCosts.text;
-
-    return { body: { response_type: 'in_channel', text: "Datadog projected cost this month $" + cost.toFixed(2) + " Projected cost next month $" + forwardCost.toFixed(2) + "\n\nDetail: " + detail } };
-}
-
-function main(params) {
-  if (!params.__secrets || !params.__secrets.datadogApiKey || !params.__secrets.datadogApplicationKey) {
-    return { body: { text: "You must create secrets for datadogApiKey and datadogApplicationKey to use this command " } };
+  if (hostsCosts.cost !== 0) {
+    const {cost, forwardCost, ...hosts} = hostsCosts;
+    verbose.Hosts = hosts;
   }
 
-  
-  const api_key = params.__secrets.datadogApiKey;
-  const application_key = params.__secrets.datadogApplicationKey;
+  if (metricsCosts.cost !== 0) {
+    const {cost, forwardCost, ...metrics} = metricsCosts;
+    verbose.Metrics = metrics;
+  }
 
-  let now = new Date();
-  let firstOfThisMonth = new Date(now.getUTCFullYear(), now.getUTCMonth(), 1);
+  if (syntheticsCosts.cost !== 0) {
+    const {cost, forwardCost, ...synthetics} = syntheticsCosts;
+    verbose.Synthetics = synthetics;
+  }
 
-  let start_hr = firstOfThisMonth.toISOString().substring(0, 13);
-  let end_hr = now.toISOString().substring(0, 13);
+  const totalCost = hostsCosts.cost + metricsCosts.cost + syntheticsCosts.cost;
+  const totalForwardCost =
+    hostsCosts.forwardCost +
+    metricsCosts.forwardCost +
+    syntheticsCosts.forwardCost;
 
-  let query = "?api_key=" + api_key + "&application_key=" + application_key + "&start_hr=" + start_hr + "&end_hr=" + end_hr;
-  let url = "https://api.datadoghq.com/api/v1/usage/hosts";
-  return getContent(url + query).then(
-    function(hostsHtml) {
-      url = "https://api.datadoghq.com/api/v1/usage/timeseries?";
-      return getContent(url + query).then(
-        function(timeseriesHtml) {
-          url = "https://api.datadoghq.com/api/v1/usage/synthetics?";
-          return getContent(url + query).then(
-            function(syntheticsHtml) {
-              return calcCosts(JSON.parse(hostsHtml), JSON.parse(timeseriesHtml), JSON.parse(syntheticsHtml));
-            },
-            function(error) {
-              return { body: { text: "ERROR " + error} };
-            }
-          );
-        },
-        function(error) {
-          return { body: { text: "ERROR " + error} };
-        }
-      );
-    },
-    function(error) {
-      return { body: { text: "ERROR " + error} };
-    }
-  );
+  return {
+    totalCost,
+    totalForwardCost,
+    verbose
+  };
 }
+
+const _command = async (params, commandText, secrets = {}) => {
+  const {datadogApiKey, datadogApplicationKey} = secrets;
+  if (!datadogApiKey || !datadogApplicationKey) {
+    return {
+      text:
+        `You need \`datadogApiKey\` and \`datadogApplicationKey\` secrets to use this command.` +
+        `Create them by running \`/nc secret_create\``
+    };
+  }
+
+  const {detail = false} = params;
+  const result = [];
+
+  const now = new Date();
+  const firstOfThisMonth = new Date(now.getUTCFullYear(), now.getUTCMonth(), 1);
+  const startHour = firstOfThisMonth.toISOString().slice(0, 13);
+  const endHour = now.toISOString().slice(0, 13);
+
+  const URL = 'https://api.datadoghq.com/api/v1/usage';
+
+  const query =
+    '?api_key=' +
+    datadogApiKey +
+    '&application_key=' +
+    datadogApplicationKey +
+    '&start_hr=' +
+    startHour +
+    '&end_hr=' +
+    endHour;
+
+  try {
+    const [hostsData, timeseriesData, syntheticsData] = await Promise.all([
+      getContent(URL + '/hosts' + query),
+      getContent(URL + '/timeseries' + query),
+      getContent(URL + '/synthetics' + query)
+    ]);
+
+    const {totalCost, totalForwardCost, verbose} = calcCosts(
+      JSON.parse(hostsData),
+      JSON.parse(timeseriesData),
+      JSON.parse(syntheticsData)
+    );
+
+    result.push(
+      mui(
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `*Datadog Bill*`
+          }
+        },
+        client
+      )
+    );
+
+    result.push(mui({type: 'divider'}, client));
+
+    result.push(
+      mui(
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `Projected this month: *$${totalCost.toFixed(
+              2
+            )}* Projected next month: *$${totalForwardCost.toFixed(2)}*`
+          }
+        },
+        client
+      )
+    );
+
+    if (detail === true) {
+      for (const [key, service] of Object.entries(verbose)) {
+        result.push(
+          mui(
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: `*${key}*`
+              }
+            },
+            client
+          )
+        );
+
+        const detailSection = {type: 'section', fields: []};
+        for (const [key, value] of Object.entries(service)) {
+          detailSection.fields.push({
+            type: 'mrkdwn',
+            text: `${key}: \`${value}\``
+          });
+        }
+
+        result.push(mui(detailSection, client));
+      }
+    }
+  } catch (error) {
+    result.push(
+      mui(
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `*Error*: ${error.message}`
+          }
+        },
+        client
+      )
+    );
+  }
+
+  return {
+    response_type: 'in_channel',
+    [client === 'slack' ? 'blocks' : 'text']:
+      client === 'slack' ? result : result.join('\n')
+  };
+};
+
+const main = async ({__secrets = {}, commandText, ...params}) => ({
+  body: await _command(params, commandText, __secrets)
+});
+module.exports = main;
