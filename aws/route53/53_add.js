d2abd0ba1a8842e7d26b68cae3cbee8e965edf06
Satya Rohith <me@satyarohith.com>
2020-01-31 08:06:28 +0530

    fix(53_add): handle slack escaping

--- aws/route53/53_add.js
+++ aws/route53/53_add.js
@@ -1,17 +1,19 @@
 // jshint esversion: 9
 
 async function _command(params, commandText, secrets = {}) {
-  const {
-    hostname,
-    ip_address
-  } = params;
-  
-  if (!secrets.route53AccessKey || !secrets.route53SecretKey || !secrets.route53ZoneId) {
+  const {route53AccessKey, route53SecretKey, route53ZoneId} = secrets;
+  if (!route53AccessKey || !route53SecretKey || !route53ZoneId) {
     return {
       response_type: 'in_channel', // or `ephemeral` for private response
-      text: "You must create secrets for route53AccessKey, route53SecretKey and route53ZoneId to use this command"
+      text:
+        'You must create secrets for route53AccessKey, route53SecretKey and route53ZoneId to use this command'
     };
   }
+
+  let {hostname, ip_address} = params;
+  hostname = hostname.startsWith('<')
+    ? hostname.split('|')[1].slice(0, -1)
+    : hostname;
 
   const AWS = require('aws-sdk');
 
@@ -21,36 +23,45 @@ async function _command(params, commandText, secrets = {}) {
   });
 
   let changeParams = {
-     "HostedZoneId": '/hostedzone/' + secrets.route53ZoneId,
-     "ChangeBatch": {
-       "Changes": [
-         {
-           "Action": "UPSERT",
-           "ResourceRecordSet": {
-             "Name": hostname,
-             "Type": 'A',
-             "TTL": 299,
-             "ResourceRecords": [ { "Value": ip_address } ]
-           }
-         }
-       ]
-     }
-   };
-  return route53.changeResourceRecordSets(changeParams).promise().then(
-    function(data) {
-      return {
-        "response_type": "in_channel",
-        "text": 'hostname ' + hostname + ' added. Route53 status: ' + data.ChangeInfo.Status
-      };
-    },
-    function(err) {
-      return {
-        "response_type": "in_channel",
-        "text": "Error: " + JSON.stringify(err)
-      };
+    HostedZoneId: '/hostedzone/' + secrets.route53ZoneId,
+    ChangeBatch: {
+      Changes: [
+        {
+          Action: 'UPSERT',
+          ResourceRecordSet: {
+            Name: hostname,
+            Type: 'A',
+            TTL: 299,
+            ResourceRecords: [{Value: ip_address}]
+          }
+        }
+      ]
     }
-  );
+  };
+  return route53
+    .changeResourceRecordSets(changeParams)
+    .promise()
+    .then(
+      function(data) {
+        return {
+          response_type: 'in_channel',
+          text:
+            'hostname ' +
+            hostname +
+            ' added. Route53 status: ' +
+            data.ChangeInfo.Status
+        };
+      },
+      function(err) {
+        return {
+          response_type: 'in_channel',
+          text: 'Error: ' + JSON.stringify(err)
+        };
+      }
+    );
 }
 
-const main = async ({__secrets = {}, commandText, ...params}) => ({body: await _command(params, commandText, __secrets)});
+const main = async ({__secrets = {}, commandText, ...params}) => ({
+  body: await _command(params, commandText, __secrets)
+});
 module.exports = main;
