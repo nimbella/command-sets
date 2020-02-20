24ff46d89d7e0fceae9342e4016708098c67ad1f
Eric Swildens <eric_x@waba.com>
2020-02-19 16:07:51 -0800

    Update translate.js
    added help with link to list of ISO codes and conversion from language name to ISO code

--- translate/src/translate.js
+++ translate/src/translate.js
@@ -1,8 +1,19 @@
 // jshint esversion: 9
 
+async function install(pkgs) {
+  pkgs = pkgs.join(' ');
+  return new Promise((resolve, reject) => {
+    const { exec } = require('child_process');
+    exec(`npm install ${pkgs}`, (err, stdout, stderr) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+}
+
 const translator = 'https://translate.googleapis.com/translate_a/single';
 /**
- * @description null
+ * @description Translates text to a given language
  * @param {ParamsType} params list of command parameters
  * @param {?string} commandText text message
  * @param {!object} [secrets = {}] list of secrets
@@ -13,14 +24,24 @@ async function _command(params, commandText, secrets = {}) {
   const axios = require('axios');
   const sourceLang = 'auto';
   let translatedText = '';
-  const {
+  let {
     language: targetLang = 'en',
     text: sourceText
   } = params;
 
+  if (sourceText == 'help' && targetLang == 'en') {
+    return { response_type: 'in_channel', text: 'language can be a <https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes|2 character ISO6931 code> or a language name such as Spanish, Chinese, etc.' };
+  }
+
   let response;
   try {
-
+    if (targetLang.length != 2) {
+      let packages = [ 'iso-639-1' ];
+      await install(packages);
+      const ISO6391 = require('iso-639-1');
+      targetLang = ISO6391.getCode(targetLang);
+    }
+    
     const url = `${translator}?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${encodeURI(sourceText)}`;
     response = await axios.get(url);
     if (response.status !== 200) {
