95107b169230fecc1418c53aa837baa1844a0850
Satya Rohith <me@satyarohith.com>
2020-03-04 20:08:03 +0530

    feat(bluejeans): add _cancel command

--- bluejeans/README.md
+++ bluejeans/README.md
@@ -22,6 +22,12 @@ To create a meeting with `joe@example.com` & `judy@example.com`:
 ```
 Where date is in `mm/dd/yy` format and time is in UTC.
 
+To cancel a meeting:
+```sh
+/dapp bluejeans_cancel <meetingId> [<cancellationMessage>]
+```
+`meetingId` is required and `cancellationMessage` is optional.
+
 To list all meetings of admin:
 ```sh
 /dapp bluejeans_list
---
+++ bluejeans/src/bluejeans_cancel.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/**
+ * @description undefined
+ * @param {ParamsType} params list of command parameters
+ * @param {?string} commandText text message
+ * @param {!object} [secrets = {}] list of secrets
+ * @return {Promise<SlackBodyType>} Response body
+ */
+async function _command(params = {}, commandText, secrets = {}) {
+  const {bluejeansAppKey, bluejeansAppSecret} = secrets;
+  if (!bluejeansAppKey || !bluejeansAppSecret) {
+    return {
+      response_type: 'ephemeral',
+      text: `You need secrets \`bluejeansAppKey\` & \`bluejeansAppSecret\` to be able to run this command.`
+    };
+  }
+
+  const result = [];
+  const baseURL = `https://api.bluejeans.com`;
+  const {meetingId, cancellationMessage = ''} = params;
+  const axios = require('axios');
+
+  // Fetch access token
+  const {data} = await axios.post(baseURL + '/oauth2/token?Client', {
+    grant_type: 'client_credentials',
+    client_id: bluejeansAppKey,
+    client_secret: bluejeansAppSecret
+  });
+
+  // Fetch users
+  const {
+    data: {users}
+  } = await axios.get(
+    baseURL +
+      `/v1/enterprise/${data.scope.enterprise}/users?access_token=${data.access_token}`
+  );
+
+  // Cancel a meeting
+  let requestURL = baseURL + users[0].uri + `/scheduled_meeting/${meetingId}`;
+  requestURL += `?email=true&access_token=${data.access_token}`;
+
+  if (cancellationMessage) {
+    requestURL += `&cancellationMessage=${cancellationMessage}`;
+  }
+
+  const {status} = await axios.delete(requestURL);
+  if (status === 200) {
+    result.push(`Successfully cancelled the meeting (ID: \`${meetingId}\`).`);
+  }
+
+  return {
+    response_type: 'in_channel', // eslint-disable-line camelcase
+    text: result.join('\n')
+  };
+}
+
+/**
+ * @typedef {object} SlackBodyType
+ * @property {string} text
+ * @property {'in_channel'|'ephemeral'} [response_type]
+ */
+const main = async ({__secrets = {}, commandText, ...params}) => ({
+  body: await _command(params, commandText, __secrets).catch(error => ({
+    response_type: 'ephemeral',
+    text: `Error: ${error.message}`
+  }))
+});
+module.exports = main;
