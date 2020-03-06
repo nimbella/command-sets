30407be4f4edb67b3db7a336a2c563ee71e92022
Nikhil Acharya Prakash <nikhil@nimbella.com>
2020-03-06 15:52:20 +0530

    Change `'` to `"`
    Copy pasting was failing in this branch

--- bluejeans/packages/bluejeans/bluejeans.js
+++ bluejeans/packages/bluejeans/bluejeans.js
@@ -27,7 +27,7 @@ After you've the credentials, we need two secrets named \`bluejeansAppKey\` & \`
 
 To create a meeting with \`joe@example.com\` & \`judy@example.com\`:
 \`\`\`sh
-/dapp bluejeans_create -title "A new beginning" -emails "joe@example.com,judy@example.com" -start "03/01/20 18:00" -end "03/01/20 18:30'
+/dapp bluejeans_create -title "A new beginning" -emails "joe@example.com,judy@example.com" -start "03/01/20 18:00" -end "03/01/20 18:30"
 \`\`\`
 Where date is in \`mm/dd/yy\` format and time is in UTC.
 
