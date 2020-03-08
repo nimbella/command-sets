6f617123359baca25281d2638f77d493d6036556
Chandan Rai <chandan@nimbella.com>
2020-03-08 12:38:00 +0530

    fix trancate at punctuation

--- translate/packages/translate/translate.js
+++ translate/packages/translate/translate.js
@@ -46,7 +46,7 @@ async function _command(params, commandText, secrets = {}) {
       return { response_type: 'in_channel', text: 'Unknown language. Language can be a <https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes|2 character ISO6931 code> or a language name such as Spanish, Chinese, etc.' };
     }
     
-    const url = `${translator}?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${encodeURI(sourceText)}`;
+    const url = `${translator}?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q="${encodeURI(sourceText)}"`;
     response = await axios.get(url);
     if (response.status !== 200) {
       throw err;
