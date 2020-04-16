a32a841322b2713ff11e76ea5573a35802748b74
Satya Rohith <me@satyarohith.com>
2020-04-16 09:43:15 +0530

    fix(github): remove unnecessary imports

--- github/packages/github/github.js
+++ github/packages/github/github.js
@@ -7,17 +7,18 @@
  * @param {!object} [secrets = {}] list of secrets
  * @return {Promise<SlackBodyType>} Response body
  */
-const axios = require('axios');
-
 async function _command(params, commandText, secrets = {}) {
+  const baseUrl =
+    'https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots';
 
   return {
-    "blocks": [
+    blocks: [
       {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "Nimbella Commander GitHub Command Set.\
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text:
+            'Nimbella Commander GitHub Command Set.\
           \n_A Nimbella Commander command set for interfacing with GitHub._\
           \n\n*Features*:\
           \n• Create an issue\
@@ -35,127 +36,133 @@ async function _command(params, commandText, secrets = {}) {
           \nCreate secrets using.\
           \n `/nc secret_create`\
           \n<https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line|Click> to learn how to setup a Personal Access Token on GitHub\
-          \n\n\n*Usage*:"
+          \n\n\n*Usage*:'
         }
       },
-      // 
+      //
       {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "*Creating an issue:* \n`/nc github_create_issue <repo> <title> <body>`"
-        }
-      },
-      {
-        "type": "image",
-        "title": {
-          "type": "plain_text",
-          "text": "github create issue command",
-          "emoji": true
-        },
-        "image_url": "https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/create_issue.png",
-        "alt_text": "github create issue command"
-      },
-      // 
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "*Closing an issue:* \n`/nc github_close_issue <repo> <issue_number>`"
-        },
-      },
-      {
-        "type": "image",
-        "title": {
-          "type": "plain_text",
-          "text": "github close issue command",
-          "emoji": true
-        },
-        "image_url": "https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/close_issue.png",
-        "alt_text": "github close issue command"
-      },
-      
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "*Reopening an issue:* \n`/nc github_reopen_issue <repo> <issue_number>`"
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text:
+            '*Creating an issue:* \n`/nc github_create_issue <repo> <title> <body>`'
         }
       },
       {
-        "type": "image",
-        "title": {
-          "type": "plain_text",
-          "text": "github reopen issue command",
-          "emoji": true
+        type: 'image',
+        title: {
+          type: 'plain_text',
+          text: 'github create issue command',
+          emoji: true
         },
-        "image_url": "https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/reopen_issue.png",
-        "alt_text": "github reopen issue command"
+        image_url: `${baseUrl}/create_issue.png`,
+        alt_text: 'github create issue command'
       },
-      // 
+      //
       {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "*Finding a pull request by date:* \n`/nc github_find_pr <repo> <date>`"
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text:
+            '*Closing an issue:* \n`/nc github_close_issue <repo> <issue_number>`'
         }
       },
       {
-        "type": "image",
-        "title": {
-          "type": "plain_text",
-          "text": "github find_pr command",
-          "emoji": true
+        type: 'image',
+        title: {
+          type: 'plain_text',
+          text: 'github close issue command',
+          emoji: true
         },
-        "image_url": "https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/find_pr.png",
-        "alt_text": "github find_pr command"
+        image_url: `${baseUrl}/close_issue.png`,
+        alt_text: 'github close issue command'
       },
-      // 
+
       {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "*Labeling an issue or pull request:* \n`/nc github_label <repo> <number> <labels>`"
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text:
+            '*Reopening an issue:* \n`/nc github_reopen_issue <repo> <issue_number>`'
         }
       },
       {
-        "type": "image",
-        "title": {
-          "type": "plain_text",
-          "text": "github label command",
-          "emoji": true
+        type: 'image',
+        title: {
+          type: 'plain_text',
+          text: 'github reopen issue command',
+          emoji: true
         },
-        "image_url": "https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/label.png",
-        "alt_text": "github label command"
+        image_url: `${baseUrl}/reopen_issue.png`,
+        alt_text: 'github reopen issue command'
       },
-      // 
+      //
       {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "*Getting repository statistics:* \n`/nc github_stats <repo>`"
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text:
+            '*Finding a pull request by date:* \n`/nc github_find_pr <repo> <date>`'
         }
       },
       {
-        "type": "image",
-        "title": {
-          "type": "plain_text",
-          "text": "github stats command",
-          "emoji": true
+        type: 'image',
+        title: {
+          type: 'plain_text',
+          text: 'github find_pr command',
+          emoji: true
         },
-        "image_url": "https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/stats.png",
-        "alt_text": "github stats command"
+        image_url: `${baseUrl}/find_pr.png`,
+        alt_text: 'github find_pr command'
       },
-      //      
+      //
       {
-        "type": "divider"
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text:
+            '*Labeling an issue or pull request:* \n`/nc github_label <repo> <number> <labels>`'
+        }
       },
       {
-        "type": "context",
-        "elements": [
+        type: 'image',
+        title: {
+          type: 'plain_text',
+          text: 'github label command',
+          emoji: true
+        },
+        image_url: '/label.png',
+        alt_text: 'github label command'
+      },
+      //
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: '*Getting repository statistics:* \n`/nc github_stats <repo>`'
+        }
+      },
+      {
+        type: 'image',
+        title: {
+          type: 'plain_text',
+          text: 'github stats command',
+          emoji: true
+        },
+        image_url: `${baseUrl}/stats.png`,
+        alt_text: 'github stats command'
+      },
+      //
+      {
+        type: 'divider'
+      },
+      {
+        type: 'context',
+        elements: [
           {
-            "type": "mrkdwn",
-            "text": "👀 View recent pull requests with `/nc github_view_pr <repo>`\n❓Get help on these commands with `/nc github`"
+            type: 'mrkdwn',
+            text:
+              '👀 View recent pull requests with `/nc github_view_pr <repo>`\n❓Get help on these commands with `/nc github`'
           }
         ]
       }
@@ -168,9 +175,12 @@ async function _command(params, commandText, secrets = {}) {
  * @property {string} text
  * @property {'in_channel'|'ephemeral'} [response_type]
  */
-
-const main = async (args) => ({
-  body: await _command(args.params, args.commandText, args.__secrets || {}).catch(error => ({
+const main = async args => ({
+  body: await _command(
+    args.params,
+    args.commandText,
+    args.__secrets || {}
+  ).catch(error => ({
     response_type: 'ephemeral',
     text: `Error: ${error.message}`
   }))
