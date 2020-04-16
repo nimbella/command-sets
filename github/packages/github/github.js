a007cac590302573ef484d19f11bad4d091b747a
Satya Rohith <me@satyarohith.com>
2020-04-16 12:37:05 +0530

    Improve github help command (#142)
    * fix(github): remove unnecessary imports
    
    * refactor(github): avoid repetition

--- github/packages/github/github.js
+++ github/packages/github/github.js
@@ -7,155 +7,92 @@
  * @param {!object} [secrets = {}] list of secrets
  * @return {Promise<SlackBodyType>} Response body
  */
-const axios = require('axios');
-
 async function _command(params, commandText, secrets = {}) {
+  const baseUrl =
+    'https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots';
+
+  const image = (source, alt) => ({
+    type: 'image',
+    title: {
+      type: 'plain_text',
+      text: alt,
+      emoji: true
+    },
+    image_url: source,
+    alt_text: alt
+  });
+
+  const section = text => ({
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text
+    }
+  });
 
   return {
-    "blocks": [
+    response_type: 'in_channel',
+    blocks: [
+      section(
+        'Nimbella Commander GitHub Command Set.\
+      \n_A Nimbella Commander command set for interfacing with GitHub._\
+      \n\n*Features*:\
+      \n• Create an issue\
+      \n• Close an issue\
+      \n• Reopen an issue\
+      \n• Find pull requests by date\
+      \n• Label an issue/pull request\
+      \n• Request someone to review a pull request\
+      \n• View repository community statistics\
+      \n• View recent pull requests\
+      \n• View GitHub command set documentation\
+      \n\n*Requirements*:\
+      \nSet up a Personal Access Token on GitHub with repo access.\
+      \nSave the token as a secret with the key `github_token`\
+      \nCreate secrets using.\
+      \n `/nc secret_create`\
+      \n<https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line|Click> to learn how to setup a Personal Access Token on GitHub\
+      \n\n\n*Usage*:'
+      ),
+      //
+      section(
+        '*Creating an issue:* \n`/nc github_create_issue <repo> <title> <body>`'
+      ),
+      image(`${baseUrl}/create_issue.png`, 'github create issue command'),
+      //
+      section(
+        '*Closing an issue:* \n`/nc github_close_issue <repo> <issue_number>`'
+      ),
+      image(`${baseUrl}/close_issue.png`, 'github close issue command'),
+
+      section(
+        '*Reopening an issue:* \n`/nc github_reopen_issue <repo> <issue_number>`'
+      ),
+      image(`${baseUrl}/reopen_issue.png`, 'github reopen issue command'),
+      //
+      section(
+        '*Finding a pull request by date:* \n`/nc github_find_pr <repo> <date>`'
+      ),
+      image(`${baseUrl}/find_pr.png`, 'github find_pr command'),
+      //
+      section(
+        '*Labeling an issue or pull request:* \n`/nc github_label <repo> <number> <labels>`'
+      ),
+      image(`${baseUrl}/label.png`, 'github label command'),
+      //
+      section('*Getting repository statistics:* \n`/nc github_stats <repo>`'),
+      image(`${baseUrl}/stats.png`, 'github stats command'),
+      //
       {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "Nimbella Commander GitHub Command Set.\
-          \n_A Nimbella Commander command set for interfacing with GitHub._\
-          \n\n*Features*:\
-          \n• Create an issue\
-          \n• Close an issue\
-          \n• Reopen an issue\
-          \n• Find pull requests by date\
-          \n• Label an issue/pull request\
-          \n• Request someone to review a pull request\
-          \n• View repository community statistics\
-          \n• View recent pull requests\
-          \n• View GitHub command set documentation\
-          \n\n*Requirements*:\
-          \nSet up a Personal Access Token on GitHub with repo access.\
-          \nSave the token as a secret with the key `github_token`\
-          \nCreate secrets using.\
-          \n `/nc secret_create`\
-          \n<https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line|Click> to learn how to setup a Personal Access Token on GitHub\
-          \n\n\n*Usage*:"
-        }
-      },
-      // 
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "*Creating an issue:* \n`/nc github_create_issue <repo> <title> <body>`"
-        }
-      },
-      {
-        "type": "image",
-        "title": {
-          "type": "plain_text",
-          "text": "github create issue command",
-          "emoji": true
-        },
-        "image_url": "https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/create_issue.png",
-        "alt_text": "github create issue command"
-      },
-      // 
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "*Closing an issue:* \n`/nc github_close_issue <repo> <issue_number>`"
-        },
-      },
-      {
-        "type": "image",
-        "title": {
-          "type": "plain_text",
-          "text": "github close issue command",
-          "emoji": true
-        },
-        "image_url": "https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/close_issue.png",
-        "alt_text": "github close issue command"
-      },
-      
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "*Reopening an issue:* \n`/nc github_reopen_issue <repo> <issue_number>`"
-        }
+        type: 'divider'
       },
       {
-        "type": "image",
-        "title": {
-          "type": "plain_text",
-          "text": "github reopen issue command",
-          "emoji": true
-        },
-        "image_url": "https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/reopen_issue.png",
-        "alt_text": "github reopen issue command"
-      },
-      // 
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "*Finding a pull request by date:* \n`/nc github_find_pr <repo> <date>`"
-        }
-      },
-      {
-        "type": "image",
-        "title": {
-          "type": "plain_text",
-          "text": "github find_pr command",
-          "emoji": true
-        },
-        "image_url": "https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/find_pr.png",
-        "alt_text": "github find_pr command"
-      },
-      // 
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "*Labeling an issue or pull request:* \n`/nc github_label <repo> <number> <labels>`"
-        }
-      },
-      {
-        "type": "image",
-        "title": {
-          "type": "plain_text",
-          "text": "github label command",
-          "emoji": true
-        },
-        "image_url": "https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/label.png",
-        "alt_text": "github label command"
-      },
-      // 
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "*Getting repository statistics:* \n`/nc github_stats <repo>`"
-        }
-      },
-      {
-        "type": "image",
-        "title": {
-          "type": "plain_text",
-          "text": "github stats command",
-          "emoji": true
-        },
-        "image_url": "https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/stats.png",
-        "alt_text": "github stats command"
-      },
-      //      
-      {
-        "type": "divider"
-      },
-      {
-        "type": "context",
-        "elements": [
+        type: 'context',
+        elements: [
           {
-            "type": "mrkdwn",
-            "text": "👀 View recent pull requests with `/nc github_view_pr <repo>`\n❓Get help on these commands with `/nc github`"
+            type: 'mrkdwn',
+            text:
+              '👀 View recent pull requests with `/nc github_view_pr <repo>`\n❓Get help on these commands with `/nc github`'
           }
         ]
       }
@@ -168,9 +105,12 @@ async function _command(params, commandText, secrets = {}) {
  * @property {string} text
  * @property {'in_channel'|'ephemeral'} [response_type]
  */
-
-const main = async (args) => ({
-  body: await _command(args.params, args.commandText, args.__secrets || {}).catch(error => ({
+const main = async args => ({
+  body: await _command(
+    args.params,
+    args.commandText,
+    args.__secrets || {}
+  ).catch(error => ({
     response_type: 'ephemeral',
     text: `Error: ${error.message}`
   }))
