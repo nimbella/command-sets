2c97d5fb4feeebd116f2c9340157f9cc64ebcc4c
Chandan Rai <chandan@nimbella.com>
2020-04-17 15:47:32 +0530

    add token and rate limit message

--- github/packages/github/github_stats.js
+++ github/packages/github/github_stats.js
@@ -10,44 +10,70 @@
 
 const axios = require('axios');
 
-async function getRequest(url) {
-
-  return {
-    response_type: 'in_channel', // or `ephemeral` for private response
-    text: await axios.get(url)
-      .then(response => { return response.data; })
-      .catch(error => { return error.response.data; })
-  };
+const requestThreshold = 3;
+const headers = {
+  'Content-Type': 'application/json',
+};
+async function getRequest(url, secrets) {
+  if (secrets.github_token) headers.Authorization = `Bearer ${secrets.github_token}`;
+  return (axios({
+    method: 'get',
+    url,
+    headers,
+  }).then((res) => res).catch((err) => err));
 }
 
-async function _command(params, commandText, secrets = {}) {
+const section = (text) => ({
+  type: 'section',
+  text: {
+    type: 'mrkdwn',
+    text,
+  },
+});
+
+const fail = (msg) => {
+  const response = {
+    response_type: 'in_channel',
+    blocks: [section(`${msg || '*couldn\'t get the stats*'}`)],
+  };
+  return response;
+};
+
+async function command(params, commandText, secrets = {}) {
   const {
-    repo
+    repo,
   } = params;
   const url = `https://api.github.com/repos/${repo}`;
-  const data = await getRequest(url);
+  const res = await getRequest(url, secrets);
 
-  if (data.response) {
-    return {
-      response_type: 'in_channel',
-      text: data.response.headers.status
-    };
-  } else {
+  if (res && res.data) {
+    const { data } = res;
+    const tokenMessage = secrets.github_token ? '' : '*For greater limits you can add <https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line | secrets> using*\n `/nc secret_create`';
+    const currReading = parseInt(res.headers['x-ratelimit-remaining']);
+    let header = `${data.full_name} statistics`;
+    if (currReading < requestThreshold) {
+      header = `:warning: *You are about to reach the api rate limit.* ${tokenMessage}`;
+    }
+    if (currReading === 0) {
+      header = `:warning: *The api rate limit has been exhausted.* ${tokenMessage}`;
+      return fail(header);
+    }
     return {
       attachments: [{
         color: 'good',
-        title: `Most used langauge: ${data.text.language}
-Default Branch: ${data.text.default_branch}
-Collaborators: ${data.text.network_count}
-Open Issues Count: ${data.text.open_issues_count}
-Forks: ${data.text.forks}
-Watchers: ${data.text.subscribers_count}
-Stars: ${data.text.stargazers_count}`,
-        title_link: data.text.html_url,
-        pretext: `${data.text.full_name} statistics`
-      }]
+        title: `Most used langauge: ${data.language}
+Default Branch: ${data.default_branch}
+Collaborators: ${data.network_count}
+Open Issues Count: ${data.open_issues_count}
+Forks: ${data.forks}
+Watchers: ${data.subscribers_count}
+Stars: ${data.stargazers_count}`,
+        title_link: data.html_url,
+        pretext: header,
+      }],
     };
   }
+  return fail();
 }
 
 /**
@@ -57,9 +83,9 @@ Stars: ${data.text.stargazers_count}`,
  */
 
 const main = async (args) => ({
-  body: await _command(args.params, args.commandText, args.__secrets || {}).catch(error => ({
+  body: await command(args.params, args.commandText, args.__secrets || {}).catch((error) => ({
     response_type: 'ephemeral',
-    text: `Error: ${error.message}`
-  }))
+    text: `Error: ${error.message}`,
+  })),
 });
 module.exports = main;
