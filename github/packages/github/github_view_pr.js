42266d601da81be838dfa4382cddbcdba8cd5f1c
Satya Rohith <me@satyarohith.com>
2020-04-17 13:02:04 +0530

    feat(github_view_pr): support default repo

--- github/packages/github/github_view_pr.js
+++ github/packages/github/github_view_pr.js
@@ -1,83 +1,77 @@
 // jshint esversion: 9
 
 /**
- * @description null
+ * @description View recent pull requests
  * @param {ParamsType} params list of command parameters
  * @param {?string} commandText text message
  * @param {!object} [secrets = {}] list of secrets
  * @return {Promise<SlackBodyType>} Response body
  */
-const axios = require('axios');
+async function _command(params, commandText, secrets = {}) {
+  const {github_token: githubToken, github_default_repo: defaultRepo} = secrets;
+  const {repo = defaultRepo} = params;
+  if (!repo) {
+    return {
+      response_type: 'ephemeral',
+      text:
+        'Please create `github_default_repo` secret to avoid passing the repository.',
+    };
+  }
 
-const requestThreshold = 3;
-const headers = {
-  'Content-Type': 'application/json',
-};
-async function getRequest(url, secrets) {
-  if (secrets.github_token) headers.Authorization = `Bearer ${secrets.github_token}`;
-  return (axios({
-    method: 'get',
-    url,
-    headers,
-  }).then((res) => res).catch((err) => err));
-}
+  const result = [];
 
-const section = (text) => ({
-  type: 'section',
-  text: {
-    type: 'mrkdwn',
-    text,
-  },
-});
+  const tokenMessage = githubToken
+    ? ''
+    : 'For greater limits, create a secret named `github_token` with a <https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line|GitHub token> using `/nc secret_create`.';
 
-const fail = (msg) => {
-  const response = {
-    response_type: 'in_channel',
-    blocks: [section(`${msg || '*couldn\'t get to the pull requests*'}`)],
-  };
-  return response;
-};
+  try {
+    const url = `https://api.github.com/repos/${repo}/pulls?state=all`;
+    const axios = require('axios');
+    const {data} = await axios({
+      method: 'GET',
+      url: url,
+      headers: githubToken
+        ? {
+            Authorization: `Bearer ${githubToken}`,
+            'Content-Type': 'application/json',
+          }
+        : {},
+    });
 
-
-async function command(params, commandText, secrets = {}) {
-  const {
-    repo
-  } = params;
-  const url = `https://api.github.com/repos/${repo}/pulls?state=all`;
-
-  const res = await getRequest(url, secrets);
-
-  if (res && res.data) {
-    const tokenMessage = secrets.github_token ? '' : '*For greater limits you can add <https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line | secrets> using*\n `/nc secret_create`';
-    const currReading = parseInt(res.headers['x-ratelimit-remaining']);
-    let header;
+    const requestThreshold = 3;
+    const currReading = parseInt(headers['x-ratelimit-remaining']);
     if (currReading < requestThreshold) {
-      header = `:warning: *You are about to reach the api rate limit.* ${tokenMessage}`;
+      result.push({
+        pretext: `:warning: *You are about to reach the api rate limit.* ${tokenMessage}`,
+      });
     }
-    if (currReading === 0) {
-      header = `:warning: *The api rate limit has been exhausted.* ${tokenMessage}`;
-      return fail(header);
-    }
-    const pr = res.data;
-    const attachments = [];
 
-    for (let i = 0; i < pr.length && i < 10; i++) {
-      attachments.push({
+    for (const pr of data) {
+      result.push({
         color: pr[i].state == 'open' ? 'good' : 'danger',
         title: pr[i].body && !pr[i].body.includes('http') ? pr[i].body : 'Link',
         title_link: pr[i].html_url,
-        pretext: `Issue #${pr[i].number}: ${pr[i].title}\nID: ${pr[i].id} Date Created: ${pr[i].created_at}`
+        pretext: `Issue #${pr[i].number}: ${pr[i].title}\nID: ${pr[i].id} Date Created: ${pr[i].created_at}`,
       });
     }
-    if (attachments.length > 0) {
-      if (header) attachments.unshift({ pretext: header });
-      return {
-        attachments,
-      };
+  } catch (error) {
+    if (error.response.status === 404) {
+      result.push({
+        color: 'danger',
+        text: `Repository not found: <https://github.com/${repo}|${repo}>.`,
+      });
+    } else {
+      result.push({
+        color: 'danger',
+        text: `Error: ${error.response.status} ${error.response.data.message}`,
+      });
     }
-    return fail(`No pull requests could be found`);
   }
-  return fail();
+
+  return {
+    response_type: 'in_channel',
+    attachments: result,
+  };
 }
 
 /**
@@ -85,11 +79,14 @@ async function command(params, commandText, secrets = {}) {
  * @property {string} text
  * @property {'in_channel'|'ephemeral'} [response_type]
  */
-
 const main = async (args) => ({
-  body: await command(args.params, args.commandText, args.__secrets || {}).catch(error => ({
+  body: await _command(
+    args.params,
+    args.commandText,
+    args.__secrets || {}
+  ).catch((error) => ({
     response_type: 'ephemeral',
-    text: `Error: ${error.message}`
-  }))
+    text: `Error: ${error.message}`,
+  })),
 });
 module.exports = main;
