32844445e3de808a36e94142b8ef1f8b70cfb47f
Chandan Rai <chandan@nimbella.com>
2020-04-17 13:20:54 +0530

    github list command improvements

--- github/README.md
+++ github/README.md
@@ -10,6 +10,7 @@ A Nimbella Commander command set for interfacing with GitHub
 - Request someone to review a pull request
 - View repository community statistics
 - View recent pull requests
+- Finding repositories, commits, code, issues, pull requests, users and topics using keywords
 - View GitHub command set documentation
 
 ## Install
@@ -27,43 +28,59 @@ Creating an issue.
 ```
 /nc github_create_issue <repo> <title> <body>
 ```
-![GitHub create_issue command](https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/create_issue.PNG)
+![GitHub create_issue command](https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/create_issue.png)
 
 Closing an issue.
 ```
 /nc github_close_issue <repo> <issue_number>
 ```
-![GitHub close_issue command](https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/close_issue.PNG)
+![GitHub close_issue command](https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/close_issue.png)
 
 Reopening an issue
 ```
 /nc github_reopen_issue <repo> <issue_number>
 ```
-![GitHub reopen_issue command](https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/reopen_issue.PNG)
+![GitHub reopen_issue command](https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/reopen_issue.png)
 
 Finding a pull request by date.
 ```
 /nc github_find_pr <repo> <date>
 ```
-![GitHub find_pr command](https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/find_pr.PNG)
+![GitHub find_pr command](https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/find_pr.png)
 
 Labeling an issue or pull request.
 ```
 /nc github_label <repo> <number> <labels>
 ```
-![GitHub label command](https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/label.PNG)
+![GitHub label command](https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/label.png)
 
 Getting repository statistics
 ```
 /nc github_stats <repo>
 ```
-![GitHub stats command](https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/stats.PNG)
+![GitHub stats command](https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/stats.png)
 
 Viewing recent pull requests
 ```
 /nc github_view_pr <repo>
 ```
-![GitHub view_pr command](https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/view_pr.PNG)
+![GitHub view_pr command](https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/view_pr.png)
+
+Finding repositories, commits, code, issues, pull requests, users and topics using keywords
+```
+/nc github_list <entity> <keywords> [<repository>] [<language>]
+```
+![GitHub list command](https://raw.githubusercontent.com/nimbella/command-sets/master/github/screenshots/list.png)
+
+Entity can be passed in as an abbreviation:
+- r  - repositories
+- cm - commits
+- c  - code 
+- i  - issues
+- p  - pull requests
+- u  - users 
+- t  - topics
+Multiple keywords can be combined using `+` sign `github+python`
 
 Viewing GitHub command set documentation
 ```
--- github/packages/github/github_list.js
+++ github/packages/github/github_list.js
@@ -8,78 +8,79 @@
  * @return {Promise<SlackBodyType>} Response body
  */
 const axios = require('axios');
+
 const requestThreshold = 3;
 const headers = {
-  'Content-Type': 'application/json'
-}
+  'Content-Type': 'application/json',
+};
 async function getRequest(url, secrets) {
-  if (secrets.github_token)
-    headers.Authorization = `Bearer ${secrets.github_token}`;
+  if (secrets.github_token) headers.Authorization = `Bearer ${secrets.github_token}`;
   return (axios({
-    method: "get",
-    url: url,
-    headers: headers
+    method: 'get',
+    url,
+    headers,
   })
-    .then(res => { return res; })
-    .catch(err => { return err; }));
+    .then((res) => res)
+    .catch((err) => err));
 }
 
-async function _command(params, commandText, secrets = {}) {
+async function command(params, commandText, secrets = {}) {
   let {
-    entity, //repositories, commits, code, issues pull requests, users, topics
+    entity, // repositories, commits, code, issues pull requests, users, topics
     keywords,
     repository,
-    language
+    language,
   } = params;
   let displayEntity = entity;
-  let displayKeywords = keywords;
-  let pageSize = ''
+  const displayKeywords = keywords;
+  let pageSize = '';
   switch (entity) {
     case 'r':
     case 'rep':
     case 'repo':
     case 'repos':
-      displayEntity = 'Repositories'
-      entity = 'repositories'
+      displayEntity = 'Repositories';
+      entity = 'repositories';
       break;
     case 'cm':
     case 'commit':
-      entity = 'commits'
-      displayEntity = 'Commits'
-      headers.Accept = 'application/vnd.github.cloak-preview'
+      entity = 'commits';
+      displayEntity = 'Commits';
+      headers.Accept = 'application/vnd.github.cloak-preview';
       break;
     case 'c':
     case 'cd':
-      entity = 'code'
-      displayEntity = 'Code Files'
-      keywords = keywords + `+in:file`
-      if (!repository) return fail('please specify a repository')
+      entity = 'code';
+      displayEntity = 'Code Files';
+      keywords += '+in:file';
+      if (!repository) return fail('please specify a repository');
       break;
     case 'i':
     case 'issue':
-      entity = 'issues'
-      displayEntity = 'Issues'
-      keywords = keywords + `+is:issue`
-      pageSize = '&per_page=10"'
+      entity = 'issues';
+      displayEntity = 'Issues';
+      keywords += '+is:issue';
+      pageSize = '&per_page=8';
       break;
     case 'p':
     case 'pr':
     case 'prs':
-      entity = 'issues'
-      displayEntity = 'Pull-requests'
-      keywords = keywords + `+is:pr`
+      entity = 'issues';
+      displayEntity = 'Pull-requests';
+      keywords += '+is:pr';
+      pageSize = '&per_page=20';
       break;
     case 'u':
     case 'user':
-      displayEntity = 'Users'
-      entity = 'users'
+      displayEntity = 'Users';
+      entity = 'users';
       break;
     case 't':
     case 'topic':
-      entity = 'topics'
-      displayEntity = 'Topics'
-      headers.Accept = 'application/vnd.github.mercy-preview+json'
-      pageSize = '&per_page=10"'
+      entity = 'topics';
+      displayEntity = 'Topics';
+      headers.Accept = 'application/vnd.github.mercy-preview+json';
+      pageSize = '&per_page=10';
       break;
     default:
       break;
@@ -88,52 +89,50 @@ async function _command(params, commandText, secrets = {}) {
   const res = await getRequest(url, secrets);
 
   if (res && res.data) {
-    const token_message = secrets.github_token ? '' : '*For greater limits you can add <https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line | secrets> using*\n `/nc secret_create`'
-    const curr_reading = parseInt(res.headers['x-ratelimit-remaining'])
-    let header = `*${displayEntity}* with keywords _*${displayKeywords}*_`
-    if (curr_reading < requestThreshold) {
-      header = `:warning: *You are about to reach the api rate limit.* ${token_message}`
+    const tokenMessage = secrets.github_token ? '' : '*For greater limits you can add <https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line | secrets> using*\n `/nc secret_create`';
+    const currReading = parseInt(res.headers['x-ratelimit-remaining']);
+    let header = `*${displayEntity}* with keywords _*${displayKeywords}*_`;
+    if (currReading < requestThreshold) {
+      header = `:warning: *You are about to reach the api rate limit.* ${tokenMessage}`;
     }
-    if (curr_reading === 0) {
-      header = `:warning: *The api rate limit has been exhausted.* ${token_message}`
+    if (currReading === 0) {
+      header = `:warning: *The api rate limit has been exhausted.* ${tokenMessage}`;
       return fail(header);
     }
     return success(entity, header, res.data.items || []);
   }
-  else {
-    return fail();
-  }
 
+  return fail();
 }
 
 
 const image = (source, alt) => ({
   type: 'image',
   image_url: source,
-  alt_text: alt
+  alt_text: alt,
 });
 
-const section = text => ({
+const section = (text) => ({
   type: 'section',
   text: {
     type: 'mrkdwn',
-    text
-  }
+    text,
+  },
 });
 
-const divider = _ => ({
-  type: "divider"
-})
+const divider = (_) => ({
+  type: 'divider',
+});
 
 const fail = (msg) => {
   const response = {
     response_type: 'in_channel',
-    blocks: [section(`*${msg || `couldn\'t get the listing`}*`)]
+    blocks: [section(`*${msg || 'couldn\'t get the listing'}*`)],
   };
   return response;
-}
+};
 
-const repositories = (items, response) => (items).forEach(item => {
+const repositories = (items, response) => (items).forEach((item) => {
   response.blocks.push(
     {
       type: 'section',
@@ -150,10 +149,10 @@ const repositories = (items, response) => (items).forEach(item => {
       ],
     },
   );
-})
+});
 
-const commits = (items, response) => (items).forEach(item => {
-  item.author = item.author || item.committer
+const commits = (items, response) => (items).forEach((item) => {
+  item.author = item.author || item.committer;
   response.blocks.push(
     {
       type: 'section',
@@ -162,14 +161,14 @@ const commits = (items, response) => (items).forEach(item => {
         {
           type: 'mrkdwn',
           text: `*Repository:* <${item.repository.html_url}|${item.repository.full_name}> \n *Author:* <${item.author.url}|${item.commit.author.name}> \n *Committer:* <${item.committer.url}|${item.commit.committer.name}> \n *Comments:* ${item.commit.comment_count}`,
-        }
+        },
       ],
     },
   );
   response.blocks.push(section(`<${item.html_url}| Commit> Message: ${item.commit.message}`));
-})
+});
 
-const code = (items, response) => (items).forEach(item => {
+const code = (items, response) => (items).forEach((item) => {
   response.blocks.push(
     {
       type: 'section',
@@ -186,11 +185,10 @@ const code = (items, response) => (items).forEach(item => {
       ],
     },
   );
-})
+});
 
-const issues = (items, response) => (items).forEach(item => {
-  item.author = item.author || item.user
-  response.blocks.push(divider());
+const issues = (items, response) => (items).forEach((item) => {
+  item.author = item.author || item.user;
   response.blocks.push(
     {
       type: 'section',
@@ -198,19 +196,19 @@ const issues = (items, response) => (items).forEach(item => {
       fields: [
         {
           type: 'mrkdwn',
-          text: `*<${item.html_url}|Title>:* ${item.title} \n *Author:* <${item.author.url}|${item.author.login}>  ${item.assignee ? `\n *Assignee:* <${item.assignee.url}|${item.assignee.login}>` : ``}  \n *Comments:* ${item.comments}`,
+          text: `*<${item.html_url}|Title>:* ${item.title} \n *Author:* <${item.author.url}|${item.author.login}>  ${item.assignee ? `\n *Assignee:* <${item.assignee.url}|${item.assignee.login}>` : ''}  \n *Comments:* ${item.comments}`,
         },
         {
           type: 'mrkdwn',
-          text: `*Labels:* ${item.labels.map(l => { return `\n<${l.url}|${l.name}>` })}`
-        }
+          text: `*Labels:* ${item.labels.map((l) => `\n<${l.url}|${l.name}>`)}`,
+        },
       ],
-    }
+    },
   );
-  response.blocks.push(section(item.body));
-})
+  if (item.body) response.blocks.push(section(item.body));
+});
 
-const users = (items, response) => (items).forEach(item => {
+const users = (items, response) => (items).forEach((item) => {
   response.blocks.push(
     {
       type: 'section',
@@ -219,26 +217,26 @@ const users = (items, response) => (items).forEach(item => {
         {
           type: 'mrkdwn', // TODO: Add more useful information
           text: `${item.type === 'User' ? ':male-office-worker:' : ':office:'} <${item.html_url}|${item.login}>\n<${item.html_url}?tab=repositories|repositories> \n<${item.html_url}?tab=projects|projects> \n<${item.html_url}?tab=stars|stars>  \n<${item.html_url}?tab=following|following> \n<${item.html_url}?tab=followers|followers>`,
-        }
+        },
       ],
     },
   );
-})
+});
 
-const topics = (items, response) => (items).forEach(item => {
+const topics = (items, response) => (items).forEach((item) => {
   response.blocks.push(
     {
       type: 'section',
       fields: [
         {
           type: 'mrkdwn',
-          text: `*${item.display_name || item.name}* ${item.short_description ? '\n' + item.short_description : ''} ${item.created_by ? `\n *Creator:* ${item.created_by}` : ''} ${item.released ? `\n *Released:* ${item.released}` : ''} \n *Created:* <!date^${Math.floor(new Date(item.created_at).getTime() / 1000)}^{date_long_pretty} at {time}|${item.created_at}>  \n *Updated:* <!date^${Math.floor(new Date(item.updated_at).getTime() / 1000)}^{date_long_pretty} at {time}|${item.updated_at}>  \n *Featured:* ${item.featured ? ':thumbsup:' : ':thumbsdown:'}  *Curated:* ${item.curated ? ':thumbsup:' : ':thumbsdown:'}`,
-        }
+          text: `*${item.display_name || item.name}* ${item.short_description ? `\n${item.short_description}` : ''} ${item.created_by ? `\n *Creator:* ${item.created_by}` : ''} ${item.released ? `\n *Released:* ${item.released}` : ''} \n *Created:* <!date^${Math.floor(new Date(item.created_at).getTime() / 1000)}^{date_long_pretty} at {time}|${item.created_at}>  \n *Updated:* <!date^${Math.floor(new Date(item.updated_at).getTime() / 1000)}^{date_long_pretty} at {time}|${item.updated_at}>  \n *Featured:* ${item.featured ? ':thumbsup:' : ':thumbsdown:'}  *Curated:* ${item.curated ? ':thumbsup:' : ':thumbsdown:'}`,
+        },
       ],
     },
   );
   if (item.description) response.blocks.push(section(item.description));
-})
+});
 
 const success = (entity, header, items) => {
   const response = {
@@ -252,29 +250,23 @@ const success = (entity, header, items) => {
     },
     ],
   };
-  if (entity === 'repositories')
-    repositories(items, response);
-  if (entity === 'commits')
-    commits(items, response);
-  if (entity === 'code')
-    code(items, response);
-  if (entity === 'issues')
-    issues(items, response);
-  if (entity === 'users')
-    users(items, response);
-  if (entity === 'topics')
-    topics(items, response);
+  if (entity === 'repositories') repositories(items, response);
+  if (entity === 'commits') commits(items, response);
+  if (entity === 'code') code(items, response);
+  if (entity === 'issues') issues(items, response);
+  if (entity === 'users') users(items, response);
+  if (entity === 'topics') topics(items, response);
 
   response.blocks.push({
     type: 'context',
     elements: [{
       type: 'mrkdwn',
       text: 'add _github command-set_ to your Slack with <https://nimbella.com/product/commander/ | Commander>',
-    }]
-  })
+    }],
+  });
 
   return response;
-}
+};
 /**
  * @typedef {object} SlackBodyType
  * @property {string} text
@@ -282,9 +274,9 @@ const success = (entity, header, items) => {
  */
 
 const main = async (args) => ({
-  body: await _command(args.params, args.commandText, args.__secrets || {}).catch(error => ({
+  body: await command(args.params, args.commandText, args.__secrets || {}).catch((error) => ({
     response_type: 'ephemeral',
-    text: `Error: ${error.message}`
-  }))
+    text: `Error: ${error.message}`,
+  })),
 });
 module.exports = main;
