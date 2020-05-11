a61b5239d945c9b74d746d2c69d03f5a6c5a7bed
Satya Rohith <me@satyarohith.com>
2020-05-11 23:02:00 +0530

    Add netlify command set (#169)

---
+++ netlify/packages/netlify/list_deploys.js
@@ -0,0 +1,94 @@
+// jshint esversion: 9
+
+/**
+ * @description Access all deploys of a site.
+ * @param {ParamsType} params list of command parameters
+ * @param {?string} commandText text message
+ * @param {!object} [secrets = {}] list of secrets
+ * @return {Promise<SlackBodyType>} Response body
+ */
+async function _command(params, commandText, secrets = {}) {
+  const {netlifyToken} = secrets;
+  if (!netlifyToken) {
+    return {
+      response_type: 'ephemeral',
+      text:
+        'A secret named `netlifyToken` is required to run this command. Grab your Netlify token and run `/nc secret_create` to get a link to the secret creator.'
+    };
+  }
+
+  let {site, skip = 0} = params;
+  // Handle slack escaping
+  site = site.startsWith('<') ? site.split('|')[1].slice(0, -1) : site;
+  skip = skip === false ? 0 : skip;
+
+  const axios = require('axios');
+  const result = [];
+
+  try {
+    const URL = `https://api.netlify.com/api/v1/sites/${site}/deploys?access_token=${netlifyToken}`;
+    const {data} = await axios.get(URL);
+
+    for (
+      let i = skip > data.length ? 0 : skip;
+      i < (data.length < 20 ? data.length : 20);
+      i++
+    ) {
+      const site = data[i];
+      const body = [
+        `Deploy ID: \`${site.id}\``,
+        `Build ID: \`${site.build_id}\``,
+        `State: \`${site.state}\``
+      ];
+
+      if (site.commit_url) {
+        body.push(
+          `Commit: \`<${site.commit_url}|${site.commit_ref.slice(0, 7)}>\``
+        );
+      }
+
+      if (site.deploy_time) {
+        body.push(`Deploy Time: ${site.deploy_time + 's' || 'not available'}`);
+      }
+
+      if (site.updated_at) {
+        `Deployed on <!date^${Number(
+          new Date(site.updated_at).getTime().toString().slice(0, -3)
+        )}^{date_short} at {time}|${site.updated_at}>`;
+      }
+
+      result.push({
+        color: site.state === 'error' ? 'danger' : 'good',
+        title: site.title,
+        text: body.join('\n')
+      });
+    }
+  } catch (error) {
+    result.push({
+      color: 'danger',
+      text: `Error: ${error.message}`
+    });
+  }
+
+  return {
+    response_type: 'in_channel',
+    attachments: result
+  };
+}
+
+/**
+ * @typedef {object} SlackBodyType
+ * @property {string} text
+ * @property {'in_channel'|'ephemeral'} [response_type]
+ */
+const main = async args => ({
+  body: await _command(
+    args.params,
+    args.commandText,
+    args.__secrets || {}
+  ).catch(error => ({
+    response_type: 'ephemeral',
+    text: `Error: ${error.message}`
+  }))
+});
+module.exports = main;
---
+++ netlify/packages/netlify/list_sites.js
@@ -0,0 +1,124 @@
+// jshint esversion: 9
+
+/**
+ * @description null
+ * @param {ParamsType} params list of command parameters
+ * @param {?string} commandText text message
+ * @param {!object} [secrets = {}] list of secrets
+ * @return {Promise<SlackBodyType>} Response body
+ */
+async function _command(params, commandText, secrets = {}) {
+  const {netlifyToken} = secrets;
+  if (!netlifyToken) {
+    return {
+      response_type: 'ephemeral',
+      text:
+        'A secret named `netlifyToken` is required to run this command. Grab your Netlify token and run `/nc secret_create` to get a link to the secret creator.'
+    };
+  }
+
+  let {skip} = params;
+  skip = skip === false ? 0 : Number(skip);
+
+  const axios = require('axios');
+  const result = [];
+
+  try {
+    const URL = `https://api.netlify.com/api/v1/sites?access_token=${netlifyToken}`;
+    const {data} = await axios.get(URL);
+
+    const loopLength = data.length < skip + 5 ? data.length : skip + 5;
+    for (let i = skip; i < loopLength; i++) {
+      const site = data[i];
+      const body = [
+        `*<${site.ssl_url}|${
+          site.custom_domain ? site.custom_domain : site.name
+        }>*`,
+        `Site ID: \`${site.site_id}\``,
+        `Managed DNS: \`${site.managed_dns}\``
+      ];
+
+      if (site.published_deploy && site.published_deploy.commit_url) {
+        body.push(
+          `Deploy commit: \`<${
+            site.published_deploy.commit_url
+          }|${site.published_deploy.commit_ref.slice(0, 7)}>\``
+        );
+      }
+
+      if (site.deploy_id) {
+        body.push(`Deploy ID: \`${site.deploy_id}\``);
+      }
+
+      if (site.published_deploy) {
+        body.push(`Deploy URL: \`${site.published_deploy.deploy_ssl_url}\``);
+        body.push(
+          `Last deployed ${
+            site.committer
+              ? 'by ' +
+                `<https://github.com/${site.committer}|${site.committer} `
+              : ''
+          }on *<!date^${new Date(site.published_deploy.published_at)
+            .getTime()
+            .toString()
+            .slice(0, -3)}^{date_short} at {time}|${
+            site.published_deploy.published_at
+          }>*`
+        );
+      }
+
+      const section = {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: body.join('\n')
+        }
+      };
+
+      if (site.screenshot_url) {
+        section.accessory = {
+          type: 'image',
+          image_url: site.screenshot_url,
+          alt_text: site.name
+        };
+      }
+
+      result.push(section);
+    }
+
+    result.push({
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `Showing: ${skip + 1}-${loopLength} Total: ${data.length}`
+      }
+    });
+  } catch (error) {
+    result.push({
+      color: 'danger',
+      text: `Error: ${error.message}`
+    });
+  }
+
+  return {
+    response_type: 'in_channel',
+    blocks: result
+  };
+}
+
+/**
+ * @typedef {object} SlackBodyType
+ * @property {string} text
+ * @property {'in_channel'|'ephemeral'} [response_type]
+ */
+const main = async args => ({
+  body: await _command(
+    args.params,
+    args.commandText,
+    args.__secrets || {}
+  ).catch(error => ({
+    response_type: 'ephemeral',
+    text: `Error: ${error.message}`
+  }))
+});
+module.exports = main;
