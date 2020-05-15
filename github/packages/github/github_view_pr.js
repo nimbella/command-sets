36ae9780c5dd03e1bd7927fff8921ec04024a02c
Satya Rohith <me@satyarohith.com>
2020-05-15 12:30:59 +0530

    fix(github_view_pr): fix undefined issue

--- github/packages/github/github_view_pr.js
+++ github/packages/github/github_view_pr.js
@@ -8,13 +8,16 @@
  * @return {Promise<SlackBodyType>} Response body
  */
 async function _command(params, commandText, secrets = {}) {
-  const {github_token: githubToken, github_default_repo: defaultRepo} = secrets;
+  const {
+    github_token: githubToken,
+    github_default_repo: defaultRepo = 'nimbella/command-sets'
+  } = secrets;
   const {repo = defaultRepo, state = 'open'} = params;
   if (!repo) {
     return {
       response_type: 'ephemeral',
       text:
-        'Either pass a repo name or create a secret named `github_default_repo` to avoid passing the repository.',
+        'Either pass a repo name or create a secret named `github_default_repo` to avoid passing the repository.'
     };
   }
 
@@ -27,22 +30,22 @@ async function _command(params, commandText, secrets = {}) {
   try {
     const url = `https://api.github.com/repos/${repo}/pulls?state=${state}`;
     const axios = require('axios');
-    const {data} = await axios({
+    const {data, headers} = await axios({
       method: 'GET',
       url: url,
       headers: githubToken
         ? {
             Authorization: `Bearer ${githubToken}`,
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json'
           }
-        : {},
+        : {}
     });
 
     const requestThreshold = 3;
     const currReading = parseInt(headers['x-ratelimit-remaining']);
     if (currReading < requestThreshold) {
       result.push({
-        pretext: `:warning: *You are about to reach the api rate limit.* ${tokenMessage}`,
+        pretext: `:warning: *You are about to reach the api rate limit.* ${tokenMessage}`
       });
     }
 
@@ -66,31 +69,33 @@ async function _command(params, commandText, secrets = {}) {
         text: `_Last on <!date^${
           new Date(pr.created_at).getTime() / 1000
         }^{date_short} at {time}|${pr.created_at}>_\n${body}`,
-        title_link: pr.html_url,
+        title_link: pr.html_url
       });
     }
   } catch (error) {
+    console.log(error);
+
     if (error.response.status === 403) {
       result.push({
         color: 'danger',
-        text: `:warning: *The api rate limit has been exhausted.* ${tokenMessage}`,
+        text: `:warning: *The api rate limit has been exhausted.* ${tokenMessage}`
       });
     } else if (error.response.status === 404) {
       result.push({
         color: 'danger',
-        text: `Repository not found: <https://github.com/${repo}|${repo}>.`,
+        text: `Repository not found: <https://github.com/${repo}|${repo}>.`
       });
     } else {
       result.push({
         color: 'danger',
-        text: `Error: ${error.response.status} ${error.response.data.message}`,
+        text: `Error: ${error.response.status} ${error.response.data.message}`
       });
     }
   }
 
   return {
     response_type: 'in_channel',
-    attachments: result,
+    attachments: result
   };
 }
 
@@ -99,14 +104,14 @@ async function _command(params, commandText, secrets = {}) {
  * @property {string} text
  * @property {'in_channel'|'ephemeral'} [response_type]
  */
-const main = async (args) => ({
+const main = async args => ({
   body: await _command(
     args.params,
     args.commandText,
     args.__secrets || {}
-  ).catch((error) => ({
+  ).catch(error => ({
     response_type: 'ephemeral',
-    text: `Error: ${error.message}`,
-  })),
+    text: `Error: ${error.message}`
+  }))
 });
 module.exports = main;
