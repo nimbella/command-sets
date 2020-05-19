8b478d5b3d20b16fd5ac17a8d8fb883daf50bdb7
Satya Rohith <me@satyarohith.com>
2020-05-19 15:02:45 +0530

    feat(github_view_pr): support multiple repos

--- github/packages/github/github_view_pr.js
+++ github/packages/github/github_view_pr.js
@@ -8,15 +8,19 @@
  * @return {Promise<SlackBodyType>} Response body
  */
 async function _command(params, commandText, secrets = {}) {
-  const {github_token: githubToken, github_default_repo: defaultRepo} = secrets;
-  const {repo = defaultRepo, state = 'open'} = params;
-  if (!repo) {
+  let {github_token: githubToken, github_repos: githubRepos} = secrets;
+  githubRepos = params.repo ? params.repo : githubRepos;
+
+  const {state = 'open'} = params;
+  if (!githubRepos) {
     return {
       response_type: 'ephemeral',
       text:
         'Either pass a repo name or create a secret named `github_default_repo` to avoid passing the repository.'
     };
   }
+
+  githubRepos = githubRepos.split(',').map(repo => repo.trim());
 
   const result = [];
 
@@ -25,67 +29,82 @@ async function _command(params, commandText, secrets = {}) {
     : 'For greater limits, create a secret named `github_token` with a <https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line|GitHub token> using `/nc secret_create`.';
 
   try {
-    const url = `https://api.github.com/repos/${repo}/pulls?state=${state}`;
     const axios = require('axios');
-    const {data, headers} = await axios({
-      method: 'GET',
-      url: url,
-      headers: githubToken
-        ? {
-            Authorization: `Bearer ${githubToken}`,
-            'Content-Type': 'application/json'
-          }
-        : {}
-    });
-
-    const requestThreshold = 3;
-    const currReading = parseInt(headers['x-ratelimit-remaining']);
-    if (currReading < requestThreshold) {
-      result.push({
-        pretext: `:warning: *You are about to reach the api rate limit.* ${tokenMessage}`
-      });
+    const networkRequests = [];
+    for (const repo of githubRepos) {
+      const url = `https://api.github.com/repos/${repo}/pulls?state=${state}`;
+      networkRequests.push(
+        axios({
+          method: 'GET',
+          url: url,
+          headers: githubToken
+            ? {
+                Authorization: `Bearer ${githubToken}`,
+                'Content-Type': 'application/json'
+              }
+            : {}
+        })
+      );
     }
 
-    // Matches html tags
-    const html = new RegExp(/<.*>.*<\/.*>/);
-    for (const pr of data) {
-      const body = html.test(pr.body)
-        ? `_couldn't render body of pr_`
-        : pr.body
-            // Convert markdown links to slack format.
-            .replace(/!*\[(.*)\]\((.*)\)/g, '<$2|$1>')
-            // Covert Issues mentions to links
-            .replace(/#(\d+)/g, `<https://github.com/${repo}/issues/$1|#$1>`)
-            // Replace markdown headings with slack bold
-            .replace(/#+\s(.+)(?:\R(?!#(?!#)).*)*/g, '*$1*');
+    const responses = await Promise.all(networkRequests);
 
+    for (let i = 0; i < responses.length; i++) {
+      const {data, headers} = responses[i];
       result.push({
-        color: pr.state === 'open' ? 'good' : 'danger',
-        title: `PR #${pr.number}: ${pr.title}`,
-        // Convert ISO to slack format.
-        text: `_Last on <!date^${
-          new Date(pr.created_at).getTime() / 1000
-        }^{date_short} at {time}|${pr.created_at}>_\n${body}`,
-        title_link: pr.html_url
+        pretext: githubRepos[i]
       });
+
+      const requestThreshold = 3;
+      const currReading = parseInt(headers['x-ratelimit-remaining']);
+      if (currReading < requestThreshold) {
+        result.push({
+          pretext: `:warning: *You are about to reach the api rate limit.* ${tokenMessage}`
+        });
+      }
+
+      // Matches <>
+      const html = new RegExp(/<.*>/);
+      for (
+        let i = 0;
+        i < (data.length > 3 && githubRepos.length > 1 ? 3 : data.length);
+        i++
+      ) {
+        const pr = data[i];
+        const body = html.test(pr.body)
+          ? `_couldn't render body of pr_`
+          : pr.body
+              // Convert markdown links to slack format.
+              .replace(/!*\[(.*)\]\((.*)\)/g, '<$2|$1>')
+              // Covert Issues mentions to links
+              .replace(
+                /#(\d+)/g,
+                `<https://github.com/${githubRepos[i]}/issues/$1|#$1>`
+              )
+              // Replace markdown headings with slack bold
+              .replace(/#+\s(.+)(?:\R(?!#(?!#)).*)*/g, '*$1*');
+
+        result.push({
+          color: pr.state === 'open' ? 'good' : 'danger',
+          title: `PR #${pr.number}: ${pr.title}`,
+          // Convert ISO to slack format.
+          text: `_Last updated on <!date^${
+            new Date(pr.created_at).getTime() / 1000
+          }^{date_short} at {time}|${pr.created_at}>_\n${body}`,
+          title_link: pr.html_url
+        });
+      }
     }
   } catch (error) {
-    console.log(error);
-
     if (error.response.status === 403) {
       result.push({
         color: 'danger',
         text: `:warning: *The api rate limit has been exhausted.* ${tokenMessage}`
       });
-    } else if (error.response.status === 404) {
-      result.push({
-        color: 'danger',
-        text: `Repository not found: <https://github.com/${repo}|${repo}>.`
-      });
     } else {
       result.push({
         color: 'danger',
-        text: `Error: ${error.response.status} ${error.response.data.message}`
+        text: `Error: ${error.response.status} ${error.response.data.message} `
       });
     }
   }
@@ -108,7 +127,7 @@ const main = async args => ({
     args.__secrets || {}
   ).catch(error => ({
     response_type: 'ephemeral',
-    text: `Error: ${error.message}`
+    text: `Error: ${error}`
   }))
 });
 module.exports = main;
