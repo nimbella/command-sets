f67b2d68c53cfb5de1b8e412b0ec340426058f27
Satya Rohith <me@satyarohith.com>
2020-07-16 18:08:36 +0530

    feat: merge awsbill with aws (#208)

--- aws/README.md
+++ aws/README.md
@@ -1,10 +1,16 @@
 # AWS Command Set
 
-Interact with Amazon Web Services directly from Slack using Nimbella Commander.
+Interact with Amazon Web Services from messaging platforms like Slack, Mattermost & Microsoft Teams using [Nimbella Commander](https://nimbella.com/product/commander).
+
+- [Installation](#Installation)
+- [Requirements](#Requirements)
+- [Commands](#Commands)
+- [Usage](#Usage)
+- [Support](#support)
 
 ## Installation
 
-Please make sure you've Commander in your [Slack team](https://slack.com/apps/AS833QXL0-nimbella-commander) before executing this Slash command.
+Please make sure you've Commander installed in your [Slack workspace](https://slack.com/apps/AS833QXL0-nimbella-commander) before executing this Slash command.
 
 ```
 /nc csm_install aws
@@ -12,27 +18,105 @@ Please make sure you've Commander in your [Slack team](https://slack.com/apps/AS
 
 ## Requirements
 
-We need three secrets named `awsAccessKey`, `awsSecretKey`, and `awsRegion` for all commands excluding route53.
+We need three secrets named `awsAccessKey`, `awsSecretKey`, and `awsRegion` for all commands excluding `route53` and `awsbill`.
 
 Watch [this](https://youtu.be/665RYobRJDY) tutorial to learn how to create AWS access keys. And while creating the keys, please make sure they have **permissions** to interact with EC2, DynamoDB, and RDS services.
 
 The values of `awsAccessKey`, `awsSecretKey`, and `awsRegion` should be your Access Key ID, Secret access key and your AWS region code respectively.
 
+**`route53`**
+
 To use `route53` commands, create three secrets named `route53AccessKey`, `route53SecretKey`, `route53ZoneId` with values being your Access Key ID, Secret access key and Route 53 Zone Identifier respectively.
+
+**`awsbill`**
+
+To use `awsbill`, We need three secrets named `awsCostExplorerAccessKeyId`, `awsCostExplorerSecretAccessKey`, and `awsCostExplorerRegion`.
+
+While creating the access keys, create a policy using the below JSON to limit the access to Cost Explorer service.
+
+```json
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "VisualEditor0",
+      "Effect": "Allow",
+      "Action": [
+        "ce:DescribeCostCategoryDefinition",
+        "ce:GetRightsizingRecommendation",
+        "ce:GetCostAndUsage",
+        "ce:GetSavingsPlansUtilization",
+        "ce:GetReservationPurchaseRecommendation",
+        "ce:ListCostCategoryDefinitions",
+        "ce:GetCostForecast",
+        "ce:GetReservationUtilization",
+        "ce:GetSavingsPlansPurchaseRecommendation",
+        "ce:GetDimensionValues",
+        "ce:GetSavingsPlansUtilizationDetails",
+        "ce:GetCostAndUsageWithResources",
+        "ce:GetReservationCoverage",
+        "ce:GetSavingsPlansCoverage",
+        "ce:GetTags",
+        "ce:GetUsageForecast"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+```
+
+The values of `awsCostExplorerAccessKeyId`, `awsCostExplorerSecretAccessKey`, and `awsCostExplorerRegion` should be your Access Key ID, Secret access key and your AWS region code respectively.
 
 ## Commands
 
-- [`ec2_list`](#ec2_list)
-- [`ec2_reboot`](#ec2_reboot)
-- [`ec2_status`](#ec2_status)
-- [`dynamodb_list`](#dynamodb_list)
-- [`rds_list`](#rds_list)
-- [`route53_list`](#route53_list)
-- [`route53_add`](#route53_add)
+- [`awsbill`](#awsbill) - Fetch AWS bill.
+- [`ec2_list`](#ec2_list) - List EC2 instances in the provided region.
+- [`ec2_reboot`](#ec2_reboot) - Reboot an EC2 instance.
+- [`ec2_status`](#ec2_status) - Know the status of EC2 instances.
+- [`dynamodb_list`](#dynamodb_list) - List Database tables.
+- [`rds_list`](#rds_list) - List Amazon RDS (Relational Database Service) instances under your account.
+- [`route53_list`](#route53_list) - List AWS route53 managed hostnames.
+- [`route53_add`](#route53_add) - Add AWS route53 hostname.
 
 ## Usage
 
-### `ec2_list`
+### [`awsbill`](packages/aws/awsbill.js)
+
+Shows your AWS bill.
+
+```sh
+/nc awsbill [<monthYear>] [-task <task>]
+```
+
+To get notified when the bill exceeds a certain amount, follow the instructions below.
+
+1. Create a secret named `awsCostThreshold` with your budget.
+
+   Run `/nc secret_create` and follow the instructions.
+
+   > The value should be a number without your currency unit. ✅ `100` ❌ `$100`
+
+   **Note**: we recommend you to set the budget a bit lesser than your actual budget since Cost Explorer API is delayed by a day.
+
+2. Setup your Slack channel with Commander
+
+   Follow this [guide](https://nimbella.com/resources-commander/guide#create-add-a-channel).
+
+3. Create a Commander task
+
+   - Run `/nc task_create awsbill #general awsbill -task` to create a task.
+
+     Replace `#general` with the slack channel you've added to commander.
+
+   - Run `/nc task_rate awsbill 1 minute` to set your task rate
+
+     This sets the task to run every minute. Learn more [here](https://nimbella.com/resources-commander/guide#view-or-set-the-task-rate).
+
+   - Run `/nc task_start awsbill`
+
+     This will start the task and now whenever the bill exceeds the set threshold, you'll get a message in your channel.
+
+### [`ec2_list`](packages/aws/ec2_list.js)
 
 Lists your EC2 instances in the provided region.
 
@@ -40,7 +124,7 @@ Lists your EC2 instances in the provided region.
 /nc ec2_list [<substr>]
 ```
 
-### `ec2_reboot`
+### [`ec2_reboot`](packages/aws/ec2_reboot.js)
 
 Reboot your EC2 instance.
 
@@ -48,7 +132,7 @@ Reboot your EC2 instance.
 /nc ec2_reboot <id>
 ```
 
-### `ec2_status`
+### [`ec2_status`](packages/aws/ec2_status.js)
 
 Know the status of your EC2 instances.
 
@@ -56,7 +140,7 @@ Know the status of your EC2 instances.
 /nc ec2_status <id>
 ```
 
-### `dynamodb_list`
+### [`dynamodb_list`](packages/aws/dynamodb_list.js)
 
 List DB Tables under your account.
 
@@ -64,7 +148,7 @@ List DB Tables under your account.
 /nc dynamodb_list [<startTable>]
 ```
 
-### `rds_list`
+### [`rds_list`](packages/aws/rds_list.js)
 
 List Amazon RDS (Relational Database Service) instances under your account.
 
@@ -72,7 +156,7 @@ List Amazon RDS (Relational Database Service) instances under your account.
 /nc rds_list
 ```
 
-### `route53_list`
+### [`route53_list`](packages/aws/route53_list.js)
 
 Lists AWS route53 managed hostnames
 
@@ -80,10 +164,14 @@ Lists AWS route53 managed hostnames
 /nc route53_list [-type <record_type>] [-match <match_string>]
 ```
 
-### `route53_add`
+### [`route53_add`](packages/aws/route53_add.js)
 
 Adds an AWS route53 hostname
 
 ```sh
 /nc route53_add <hostname> <ip_address>
 ```
+
+## Support
+
+We're always happy to help you with any issues you encounter. You may want to [join our Slack community](https://nimbella-community.slack.com/) to engage with us for a more rapid response.
---
+++ aws/packages/aws/awsbill.js
@@ -0,0 +1,319 @@
+// jshint esversion: 9
+
+/**
+ * A small function that converts slack elements `context` and `section` to mattermost compatible markdown.
+ * @param {object} element - Slack element
+ * @param {string} client - name of the client
+ */
+const mui = (element, client) => {
+  if (client !== 'mattermost') {
+    return element;
+  }
+
+  const output = [];
+  switch (element.type) {
+    case 'context': {
+      for (const item of element.elements) {
+        output.push(item.text.replace(/\*/g, '**'));
+      }
+      break;
+    }
+    case 'section': {
+      if (element.fields && element.fields.length > 0) {
+        for (const field of element.fields) {
+          output.push(field.text.replace(/\*/g, '**') + '\n');
+        }
+      } else if (element.text) {
+        // Convert single text element to h4 in mattermost.
+        output.push('#### ' + element.text.text.replace(/\*/g, '**'));
+      }
+      break;
+    }
+    case 'divider': {
+      output.push('***');
+      break;
+    }
+  }
+
+  return output.join(' ');
+};
+
+/**
+ * @description Shows your AWS bill
+ * @param {ParamsType} params list of command parameters
+ * @param {?string} commandText slack text message
+ * @param {!object} [secrets = {}] list of secrets
+ * @return {Promise<SlackBodyType>} Slack response body
+ */
+async function _command(params, commandText, secrets = {}) {
+  const {
+    awsCostExplorerAccessKeyId,
+    awsCostExplorerSecretAccessKey,
+    awsCostExplorerRegion,
+    awsCostThreshold
+  } = secrets;
+
+  if (
+    !awsCostExplorerAccessKeyId ||
+    !awsCostExplorerSecretAccessKey ||
+    !awsCostExplorerRegion
+  ) {
+    return {
+      response_type: 'ephemeral', // eslint-disable-line camelcase
+      text: `You must create secrets for \`awsCostExplorerAccessKeyId\`, \`awsCostExplorerSecretAccessKey\` and \`awsCostExplorerRegion\` to use this command`
+    };
+  }
+
+  const {month_year: monthYear, __client, task = false} = params;
+  const client = __client ? __client.name : 'slack';
+
+  if (task === true && !awsCostThreshold) {
+    return {
+      response_type: 'ephemeral', // eslint-disable-line camelcase
+      text: `Please create a secret named \`awsCostThreshold\` specifying the amount to get notified when the bill exceeds the set threshold.\nNote: The AWS API is delayed by a day, so please set your threshold amount a bit less than your actual amount.`
+    };
+  }
+
+  const AWS = require('aws-sdk');
+
+  const now = new Date();
+  let month = now.getUTCMonth();
+  let year = now.getUTCFullYear();
+
+  if (monthYear != null) {
+    const arr = monthYear.split('/');
+    if (arr.length != 2) {
+      return {
+        response_type: 'ephemeral',
+        text: 'Invalid month/year. Example: 11/2019 for November 2019'
+      };
+    }
+    month = parseInt(arr[0]) - 1;
+    year = parseInt(arr[1]);
+    if (month < 0 || month > 11 || year < 1900 || year > 2800) {
+      return {
+        response_type: 'ephemeral', // eslint-disable-line camelcase
+        text: 'Month or year out of range. Example: 11/2019 for November 2019'
+      };
+    }
+  }
+
+  // Determine billing period start/end. toISOString() is UTC (GMT) time, which is what AWS bills in
+  const firstOfThisMonth = new Date(year, month, 1);
+  const firstOfNextMonth = new Date(year, month + 1, 1);
+
+  const start = firstOfThisMonth.toISOString().substring(0, 10);
+  const end = firstOfNextMonth.toISOString().substring(0, 10);
+
+  const costParams = {
+    TimePeriod: {
+      Start: start,
+      End: end
+    },
+    Granularity: 'MONTHLY',
+    GroupBy: [{Key: 'SERVICE', Type: 'DIMENSION'}],
+    Metrics: ['AmortizedCost']
+  };
+
+  const result = {response_type: 'in_channel', blocks: []};
+
+  const costExplorer = new AWS.CostExplorer({
+    accessKeyId: secrets.awsCostExplorerAccessKeyId,
+    secretAccessKey: secrets.awsCostExplorerSecretAccessKey,
+    region: secrets.awsCostExplorerRegion
+  });
+
+  let totalCost = 0.0;
+  try {
+    const {promisify} = require('util');
+    const getCostAndUsageAsync = promisify(costExplorer.getCostAndUsage).bind(
+      costExplorer
+    );
+    const data = await getCostAndUsageAsync(costParams);
+
+    const serviceSectionList = [];
+    let serviceSection = {type: 'section', fields: []};
+    const {Groups: groups} = data.ResultsByTime[0];
+    let unit;
+    let hasMultipleUnits = false;
+
+    let serviceFieldCount = 0;
+    for (const service of groups) {
+      const cost = Number(service.Metrics.AmortizedCost.Amount);
+      if (cost === 0) {
+        continue;
+      }
+      if (serviceFieldCount == 10) {
+        serviceSectionList.push(serviceSection);
+        serviceSection = {type: 'section', fields: []};
+        serviceFieldCount = 0;
+      }
+      serviceFieldCount++;
+
+      // Object key is the search value & object value is the replace value.
+      const replaceDict = {
+        'Elastic Compute Cloud': 'Elastic Compute',
+        'Amazon ': '',
+        AWS: '',
+        'Elastic Container Service for Kubernetes': 'ECS for Kubernetes',
+        'EC2 Container Registry (ECR)': 'EC2 Container Registry'
+      };
+
+      const regex = new RegExp(Object.keys(replaceDict).join('|'), 'gi');
+      const serviceName = service.Keys[0].replace(
+        regex,
+        matched => replaceDict[matched]
+      );
+
+      totalCost += cost;
+      const {Unit: serviceUnit} = service.Metrics.AmortizedCost;
+      if (serviceUnit == 'USD') {
+        costInUnits = '$' + cost.toFixed(2);
+      } else {
+        costInUnits = cost.toFixed(2) + ' ' + serviceUnit;
+      }
+      serviceSection.fields.push({
+        type: 'mrkdwn',
+        text: costInUnits + ' *' + serviceName.trim() + '*'
+      });
+      if (unit != null && unit != serviceUnit) {
+        hasMultipleUnits = true;
+      }
+      unit = serviceUnit;
+    }
+    if (serviceFieldCount > 0) {
+      serviceSectionList.push(serviceSection);
+    }
+    totalCostString = totalCost.toFixed(2);
+    if (hasMultipleUnits) {
+      totalCostString += ' (costs in in multiple units)';
+    } else {
+      if (unit == 'USD') {
+        totalCostString = '$' + totalCostString;
+      } else {
+        totalCostString += ' ' + unit;
+      }
+    }
+
+    const months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+    ];
+
+    let title = totalCostString + ' ';
+    if (groups.length == 0) {
+      title = '*Cost information is not available for this month yet*';
+    } else if (monthYear != null) {
+      title += '*AWS Cost for ' + months[month] + ' ' + year + '*';
+    } else {
+      title += '*AWS Cost Month-to-Date*';
+    }
+
+    result.blocks.push(
+      mui(
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: title
+          }
+        },
+        client
+      )
+    );
+
+    result.blocks.push(
+      mui(
+        {
+          type: 'divider'
+        },
+        client
+      )
+    );
+    for (serviceSection of serviceSectionList) {
+      result.blocks.push(mui(serviceSection, client));
+    }
+  } catch (error) {
+    result.response_type = 'ephemeral';
+    result.blocks.push(
+      mui(
+        {
+          type: 'context',
+          elements: [{type: 'mrkdwn', text: `Error: ${error.message}`}]
+        },
+        client
+      )
+    );
+  }
+
+  if (client === 'mattermost') {
+    result.text = result.blocks.join('\n');
+    delete result.blocks;
+    if (task === true) {
+      if (Number(totalCost) >= Number(awsCostThreshold)) {
+        result.text =
+          `### The cost exceeded your threshold ${awsCostThreshold}\n` +
+          result.text +
+          `\nIncrease your threshold (${awsCostThreshold}) or stop this task to avoid getting further notifications.\n`;
+        return result;
+      } else {
+        return {text: ''};
+      }
+    } else {
+      return result;
+    }
+  }
+
+  if (task === true) {
+    if (Number(totalCost) >= Number(awsCostThreshold)) {
+      result.blocks.unshift({
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `The cost exceeded your threshold *${awsCostThreshold}*`
+        }
+      });
+
+      result.blocks.push({
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Increase your threshold (${awsCostThreshold}) or stop this task to avoid getting further notifications.`
+        }
+      });
+
+      return result;
+    } else {
+      return {text: ''};
+    }
+  } else {
+    return result;
+  }
+}
+
+/**
+ * @typedef {object} SlackBodyType
+ * @property {string} text
+ * @property {'in_channel'|'ephemeral'} [response_type]
+ */
+const main = async args => ({
+  body: await _command(
+    args.params,
+    args.commandText,
+    args.__secrets || {}
+  ).catch(error => ({
+    response_type: 'ephemeral',
+    text: `Error: ${error.message}`
+  }))
+});
+module.exports = main;
