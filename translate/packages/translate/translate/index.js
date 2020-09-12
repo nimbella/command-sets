dec0df3f5075a2e5ddbd4f8def0d9a41d5174f3c
dengliming <liming.d.pro@gmail.com>
2020-09-13 01:30:09 +0800

    Change translate command interface to accept vararg for text (#226)

--- translate/packages/translate/translate/index.js
+++ translate/packages/translate/translate/index.js
@@ -54,7 +54,8 @@ async function _command(params, commandText, secrets = {}) {
   const axios = require('axios');
   const sourceLang = 'auto';
   let translatedText = '';
-  let {language: targetLang = 'en', text: sourceText, __client} = params;
+
+  let {language: targetLang = 'en', varArgs: sourceText, __client} = params;
   const client = __client.name;
   const languageHelp =
     'Language can be a <https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes|2 character ISO6931 code> or a language name such as Spanish, Chinese, etc.';
